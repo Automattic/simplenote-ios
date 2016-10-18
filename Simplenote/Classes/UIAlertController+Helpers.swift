3119b436154e8c484151db8900df9cc208794264
Dan Roundhill <dan@automattic.com>
2016-10-18 11:12:46 -0700

    Converts to switt 3 syntax. (#92)

--- Simplenote/Classes/UIAlertController+Helpers.swift
+++ Simplenote/Classes/UIAlertController+Helpers.swift
@@ -4,19 +4,19 @@ import UIKit
 
 extension UIAlertController
 {
-    public func addCancelActionWithTitle(title: String?, handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertAction {
-        return addActionWithTitle(title, style: .Cancel, handler: handler)
+    public func addCancelActionWithTitle(_ title: String?, handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertAction {
+        return addActionWithTitle(title, style: .cancel, handler: handler)
     }
 
-    public func addDestructiveActionWithTitle(title: String?, handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertAction {
-        return addActionWithTitle(title, style: .Destructive, handler: handler)
+    public func addDestructiveActionWithTitle(_ title: String?, handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertAction {
+        return addActionWithTitle(title, style: .destructive, handler: handler)
     }
 
-    public func addDefaultActionWithTitle(title: String?, handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertAction {
-        return addActionWithTitle(title, style: .Default, handler: handler)
+    public func addDefaultActionWithTitle(_ title: String?, handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertAction {
+        return addActionWithTitle(title, style: .default, handler: handler)
     }
 
-    public func addActionWithTitle(title: String?, style: UIAlertActionStyle, handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertAction {
+    public func addActionWithTitle(_ title: String?, style: UIAlertActionStyle, handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertAction {
         let action = UIAlertAction(title: title, style: style, handler: handler)
         addAction(action)
 
@@ -28,7 +28,7 @@ extension UIAlertController
         // This method is required because the presenter ViewController must be visible, and we've got several
         // flows in which the VC that triggers the alert, might not be visible anymore.
         //
-        guard let rootViewController = UIApplication.sharedApplication().keyWindow?.rootViewController else {
+        guard let rootViewController = UIApplication.shared.keyWindow?.rootViewController else {
             print("Error loading the rootViewController")
             return
         }
@@ -38,6 +38,6 @@ extension UIAlertController
             leafViewController = leafViewController.presentedViewController!
         }
 
-        leafViewController.presentViewController(self, animated: true, completion: nil)
+        leafViewController.present(self, animated: true, completion: nil)
     }
 }
