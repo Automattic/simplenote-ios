8ff13b315c15ac653486fec7a582ee93981f5e74
Jorge Leandro Perez <jorge@lantean.co>
2016-12-23 16:30:32 -0300

    Implements SPContactsManager

---
+++ Simplenote/Classes/SPContactsManager.swift
@@ -0,0 +1,125 @@
+//
+//  SPContactsManager.swift
+//  Simplenote
+//
+//  Created by Jorge Leandro Perez on 12/23/16.
+//  Copyright © 2016 Automattic. All rights reserved.
+//
+
+import Foundation
+import Contacts
+
+
+/// Contacts Helper
+///
+class SPContactsManager: NSObject {
+
+    /// Checks if we've got permission to access the Contacts Store, or not
+    ///
+    var authorized: Bool {
+        return status == .authorized
+    }
+
+    /// All of the contacts!
+    ///
+    fileprivate var peopleCache: [PersonTag]?
+
+    /// Contacts Store Reference
+    ///
+    fileprivate let store = CNContactStore()
+
+    /// Returns the current Authorization Status
+    ///
+    fileprivate var status: CNAuthorizationStatus {
+        return CNContactStore.authorizationStatus(for: .contacts)
+    }
+
+
+    /// Whenever the Auth Status is undetermined, this helper will request access!
+    ///
+    func requestAuthorizationIfNeeded(completion: ((Bool) -> Void)?) {
+        guard status == .notDetermined else {
+            return
+        }
+
+        store.requestAccess(for: .contacts) { (success, error) in
+            completion?(success)
+        }
+    }
+
+    /// Returns the subset of Persons that contain a specified keyword, in either their email or name
+    ///
+    func people(with keyword: String) -> [PersonTag] {
+        guard let people = loadPeopleIfNeeded() else {
+            return []
+        }
+
+        return people.filter { person in
+            return person.email.contains(keyword) || person.name.contains(keyword)
+        }
+    }
+}
+
+
+
+/// Contacts Helper
+///
+private extension SPContactsManager {
+
+    /// Returns a collection of all of the PersonTag's.
+    ///
+    func loadPeopleIfNeeded() -> [PersonTag]? {
+        guard authorized else {
+            return nil
+        }
+
+        guard peopleCache == nil else {
+            return peopleCache
+        }
+
+        peopleCache = loadPeople(with: loadContacts())
+
+        return peopleCache
+    }
+
+    /// Given a collection of Contacts, this helper will return a collection of PersonTag matching instances
+    ///
+    private func loadPeople(with contacts: [CNContact]) -> [PersonTag] {
+        var persons = [PersonTag]()
+
+        for contact in contacts {
+            let fullname = CNContactFormatter.string(from: contact, style: .fullName) ?? String()
+
+            for email in contact.emailAddresses {
+                guard let person = PersonTag(name: fullname as String, email: email.value as String) else {
+                    continue
+                }
+
+                persons.append(person)
+            }
+        }
+
+        return persons
+    }
+
+    /// Returns a collection of all of the contacts: Only Fullname + Email will be loaded
+    ///
+    private func loadContacts() -> [CNContact] {
+        let keysToFetch = [
+            CNContactFormatter.descriptorForRequiredKeys(for: .fullName),
+            CNContactEmailAddressesKey as CNKeyDescriptor
+        ]
+        let request = CNContactFetchRequest(keysToFetch: keysToFetch)
+        var contacts = [CNContact]()
+
+        do {
+            try store.enumerateContacts(with: request) { (contact, _) in
+                contacts.append(contact)
+            }
+        } catch {
+            NSLog("## Error while loading contacts: \(error)")
+        }
+
+        return contacts
+    }
+}
