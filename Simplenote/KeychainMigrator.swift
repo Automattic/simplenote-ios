196d61845c57217a630f7b5d302c9fab1e3bb84e
Jorge Leandro Perez <jorge@lantean.co>
2017-11-27 11:37:59 -0300

    KeychainMigrator: Splitting into methods for testability purposes

--- Simplenote/KeychainMigrator.swift
+++ Simplenote/KeychainMigrator.swift
@@ -2,20 +2,68 @@
 //  MigrateKeychain.swift
 //  Simplenote
 //  Migrates keychain items from previous keychain group
-
+//
 import Foundation
 
-@objc class KeychainMigrator: NSObject {
-    static let oldPrefix = "4ESDVWK654."
-    static let newPrefix = "PZYM8XX95Q."
-    static let bundleId = "com.codality.NotationalFlow"
-    static let shareBundleId = bundleId + ".Share"
+
+// MARK: - KeychainMigrator
+//
+@objc
+class KeychainMigrator: NSObject {
+    /// Legacy TeamID
+    ///
+    private static let oldPrefix = "4ESDVWK654."
+
+    /// New TeamID!
+    ///
+    private static let newPrefix = "PZYM8XX95Q."
+
+    /// Main App's Bundle ID
+    ///
+    private static let bundleId = "com.codality.NotationalFlow"
+
+    /// Share Extension's Bundle ID
+    ///
+    private static let shareBundleId = bundleId + ".Share"
+
+    /// Username's User Defaults Key
+    ///
     static let usernameKey = "SPUsername"
 
+    /// Legacy TeamID Access Group
+    ///
+    static let legacyAccessGroup = oldPrefix + bundleId
+
+    /// New TeamID Access Group
+    ///
+    static let newAccessGroup = newPrefix + bundleId
+
+
+    /// Migrates the Legacy Keychain Entry over to the new Access Group
+    ///
     @objc
     static func migrateIfNecessary() {
-        guard let username = UserDefaults.standard.string(forKey: usernameKey) else {
+        guard needsPasswordMigration() else {
             return
+        }
+
+        migrateLegacyPassword()
+    }
+}
+
+
+// MARK: - Private Methods
+//
+extension KeychainMigrator {
+
+    /// Indicates if the Migration should take place. This is true whenever:
+    ///
+    /// - The Username is accessible
+    /// - There is no current password
+    ///
+    static func needsPasswordMigration() -> Bool {
+        guard let username = UserDefaults.standard.string(forKey: usernameKey) else {
+            return false
         }
 
         let newPasswordItem = KeychainPasswordItem(
@@ -23,29 +71,33 @@ import Foundation
             account: username,
             accessGroup: newPrefix + bundleId
         )
-        do {
-            try newPasswordItem.readPassword()
-            // We have the token!
-            return;
-        } catch {
-            // Looks like we need to attempt a migration...
-            let oldPasswordItem = KeychainPasswordItem(
-                service: SPCredentials.simperiumAppID(),
-                account: username,
-                accessGroup: oldPrefix + bundleId
-            )
 
-            do {
-                let password = try oldPasswordItem.readPassword()
-                let migratedPasswordItem = KeychainPasswordItem(
-                    service: SPCredentials.simperiumAppID(),
-                    account: username
-                )
-                try migratedPasswordItem.savePassword(password)
-            } catch {
-                // :(
-            }
+        return (try? newPasswordItem.readPassword()) == nil
+    }
+
+    /// Migrates the Keychain Entry associated with the Old TeamID Prefix
+    ///
+    static func migrateLegacyPassword() {
+        guard let username = UserDefaults.standard.string(forKey: usernameKey) else {
+            return
+        }
+
+        // Looks like we need to attempt a migration...
+        let oldPasswordItem = KeychainPasswordItem(
+            service: SPCredentials.simperiumAppID(),
+            account: username,
+            accessGroup: oldPrefix + bundleId
+        )
+
+        do {
+            let password = try oldPasswordItem.readPassword()
+            let migratedPasswordItem = KeychainPasswordItem(
+                service: SPCredentials.simperiumAppID(),
+                account: username
+            )
+            try migratedPasswordItem.savePassword(password)
+        } catch {
+            // :(
         }
     }
 }
-
