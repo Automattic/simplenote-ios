9250c8ceaad857059b43a9bbe1e96019a0f2decd
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-06-03 15:17:02 -0300

    Implements SPNotifications

---
+++ Simplenote/Classes/SPNotifications.h
@@ -0,0 +1,17 @@
+//
+//  SPNotifications.h
+//  Simplenote
+//
+//  Copyright © 2019 Automattic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+#pragma mark ================================================================================
+#pragma mark Notifications
+#pragma mark ================================================================================
+
+extern NSString *const SPAlphabeticalTagSortPreferenceChangedNotification;
+extern NSString *const SPCondensedNoteListPreferenceChangedNotification;
+extern NSString *const SPNotesListSortModeChangedNotification;
---
+++ Simplenote/Classes/SPNotifications.m
@@ -0,0 +1,17 @@
+//
+//  SPNotifications.m
+//  Simplenote
+//
+//  Copyright © 2019 Automattic. All rights reserved.
+//
+
+#import "SPNotifications.h"
+
+
+#pragma mark ================================================================================
+#pragma mark Notifications
+#pragma mark ================================================================================
+
+NSString *const SPAlphabeticalTagSortPreferenceChangedNotification  = @"SPAlphabeticalTagSortPreferenceChangedNotification";
+NSString *const SPCondensedNoteListPreferenceChangedNotification    = @"SPCondensedNoteListPreferenceChangedNotification";
+NSString *const SPNotesListSortModeChangedNotification              = @"SPNotesListSortModeChangedNotification";
