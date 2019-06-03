3bdc2c586db98c664a9d7aca4637c4ccb063fe43
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-06-03 13:00:00 -0300

    New Options container

---
+++ Simplenote/Options.swift
@@ -0,0 +1,67 @@
+//
+//  Options.swift
+//  Simplenote
+//
+//  Copyright © 2019 Automattic. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: - Wraps access to all of the UserDefault Values
+//
+class Options: NSObject {
+
+    /// Shared Instance
+    ///
+    @objc
+    static let shared = Options()
+
+    /// User Defaults: Convenience
+    ///
+    private var defaults: UserDefaults {
+        return UserDefaults.standard
+    }
+
+    /// Designated Initializer
+    ///
+    override private init() {
+        super.init()
+        migrateLegacyOptions()
+    }
+}
+
+
+// MARK: - Actual Options!
+//
+extension Options {
+
+    /// Returns the target Sort Mode for the Notes List
+    ///
+    @objc
+    var listSortMode: SortMode {
+        get {
+            let payload = defaults.integer(forKey: .listSortMode)
+            return SortMode(rawValue: payload) ?? .alphabeticallyAscending
+        }
+        set {
+            defaults.set(newValue.rawValue, forKey: .listSortMode)
+        }
+    }
+}
+
+
+// MARK: - Private
+//
+private extension Options {
+
+    func migrateLegacyOptions() {
+        guard let legacySortAscending: Bool = defaults[.listSortModeLegacy] else {
+            return
+        }
+
+        let newMode: SortMode = legacySortAscending ? .alphabeticallyAscending : .alphabeticallyDescending
+        defaults.set(newMode, forKey: .listSortMode)
+        defaults.removeObject(forKey: .listSortModeLegacy)
+    }
+}
