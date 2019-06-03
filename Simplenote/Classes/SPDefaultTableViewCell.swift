5df67e7e302ae6c789fc5bbe42566da3141dc31f
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-06-03 10:08:32 -0300

    Implements SPDefaultTableViewCell

---
+++ Simplenote/Classes/SPDefaultTableViewCell.swift
@@ -0,0 +1,52 @@
+//
+//  SPDefaultTableViewCell.swift
+//  Simplenote
+//
+//  Copyright © 2019 Automattic. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: - UITableViewCell with the `.default` Style
+//
+class SPDefaultTableViewCell: UITableViewCell {
+
+    /// UITableView's Reusable Identifier
+    ///
+    static let reusableIdentifier = "SPDefaultTableViewCell"
+
+    /// Designated Initializer
+    ///
+    init() {
+        super.init(style: .default, reuseIdentifier: SPDefaultTableViewCell.reusableIdentifier)
+        applySimplenoteStyle()
+    }
+
+    /// Required Initializer
+    ///
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        applySimplenoteStyle()
+    }
+}
+
+
+// MARK: - Private
+//
+private extension SPDefaultTableViewCell {
+
+    func applySimplenoteStyle() {
+        guard let theme = VSThemeManager.shared()?.theme() else {
+            fatalError()
+        }
+
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = theme.color(forKey: .noteCellBackgroundSelectionColor)
+
+        backgroundColor = theme.color(forKey: .backgroundColor)
+        selectedBackgroundView = backgroundView
+        detailTextLabel?.textColor = theme.color(forKey: .tableViewDetailTextLabelColor)
+        textLabel?.textColor = theme.color(forKey: .tableViewTextLabelColor)
+    }
+}
