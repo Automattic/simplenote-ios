cd62d02e9fa01796c6ec5afb49d5909796989105
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-06-12 11:25:33 -0300

    PlainTextExtractor: New API!

--- SimplenoteShare/Extractors/PlainTextExtractor.swift
+++ SimplenoteShare/Extractors/PlainTextExtractor.swift
@@ -6,26 +6,32 @@ import MobileCoreServices
 //
 struct PlainTextExtractor: Extractor {
 
-    ///
+    /// Accepted File Extension
     ///
     let acceptedType = kUTTypePlainText as String
 
-    ///
+    /// Indicates if a given Extension Context can be handled by the Extractor
     ///
     func canHandle(context: NSExtensionContext) -> Bool {
         return context.itemProviders(ofType: acceptedType).isEmpty == false
     }
 
+    /// Extracts a Note entity contained within a given Extension Context (If possible!)
     ///
-    ///
-    func extract(context: NSExtensionContext, onCompletion: @escaping (String?) -> Void) {
-        let providers = context.itemProviders(ofType: acceptedType)
+    func extractNote(from context: NSExtensionContext, onCompletion: @escaping (Note?) -> Void) {
+        guard let provider = context.itemProviders(ofType: acceptedType).first else {
+            onCompletion(nil)
+            return
+        }
 
-        for provider in providers {
-            provider.loadItem(forTypeIdentifier: acceptedType, options: nil) { (payload, _) in
-                let text = payload as? String
-                onCompletion(text)
+        provider.loadItem(forTypeIdentifier: acceptedType, options: nil) { (payload, _) in
+            guard let content = payload as? String else {
+                onCompletion(nil)
+                return
             }
+
+            let note = Note(content: content)
+            onCompletion(note)
         }
     }
 }
