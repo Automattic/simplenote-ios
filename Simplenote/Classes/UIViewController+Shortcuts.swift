7fa6a5b2e12d1127d65e7071d6ea6ce444f1244f
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-06-20 14:21:51 -0300

    Implements Open Note Activity

--- Simplenote/Classes/UIViewController+Shortcuts.swift
+++ Simplenote/Classes/UIViewController+Shortcuts.swift
@@ -1,4 +1,5 @@
 import Foundation
+import CoreSpotlight
 
 
 // MARK: - UIViewController Shortcuts Helpers
@@ -9,13 +10,35 @@ extension UIViewController {
     ///
     @objc
     func registerLaunchActivity() {
-        userActivity = NSUserActivity(type: .launch)
+        let title = NSLocalizedString("Open Simplenote", comment: "Siri Suggestion to open our app")
+        let activity = NSUserActivity(type: .launch, title: title)
+
+        userActivity = activity
     }
 
     /// Registers the New Note Activity
     ///
     @objc
     func registerNewNoteActivity() {
-        userActivity = NSUserActivity(type: .newNote)
+        let title = NSLocalizedString("Create a New Note", comment: "Siri Suggestion to create a New Note")
+        let activity = NSUserActivity(type: .newNote, title: title)
+
+        userActivity = activity
+    }
+
+    /// Register the Open Note Activity
+    ///
+    @objc
+    func registerOpenNoteActivity(note: Note) {
+        guard let uniqueIdentifier = note.simperiumKey, let preview = note.titlePreview else {
+            NSLog("Note with missing SimperiumKey!!")
+            return
+        }
+
+        let title = NSLocalizedString("Open \"\(preview)\"", comment: "Siri Suggestion to open a specific Note")
+        let activity = NSUserActivity(type: .openNote, title: title)
+        activity.userInfo = [CSSearchableItemActivityIdentifier: uniqueIdentifier]
+
+        userActivity = activity
     }
 }
