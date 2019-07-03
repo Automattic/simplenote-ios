f0bc658f47c30e6275d35d29c7d2875938dc6c04
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-07-03 15:47:56 -0300

    Implements UIColor Extension

---
+++ Simplenote/Classes/UIColor+Simplenote.swift
@@ -0,0 +1,38 @@
+import Foundation
+import UIKit
+
+
+// MARK: - Simplenote UIColor(s)
+//
+extension UIColor {
+
+    @objc
+    static func simplenoteGreen() -> UIColor {
+        return UIColor(red: 29.0/255.0, green: 120.0/255.0, blue: 25.0/255.0, alpha: 1.0)
+    }
+
+    @objc
+    static func simplenoteRed() -> UIColor {
+        return UIColor(red: 202.0/255.0, green: 38.0/255.0, blue: 34.0/255.0, alpha: 1.0)
+    }
+
+    @objc
+    static func simplenoteYellow() -> UIColor {
+        return UIColor(red: 214.0/255.0, green: 176.0/255.0, blue: 44.0/255.0, alpha: 1.0)
+    }
+
+    @objc
+    static func simplenoteBlue() -> UIColor {
+        return UIColor(red: 72.0/255.0, green: 149.0/255.0, blue: 217.0/255.0, alpha: 1.0)
+    }
+
+    @objc
+    static func simplenoteGray600() -> UIColor {
+        return UIColor(red: 111.0/255.0, green: 119.0/255.0, blue: 128.0/255.0, alpha: 1.0)
+    }
+
+    @objc
+    static func simplenoteGray500() -> UIColor {
+        return UIColor(red: 137.0/255.0, green: 145.0/255.0, blue: 153.0/255.0, alpha: 1.0)
+    }
+}
