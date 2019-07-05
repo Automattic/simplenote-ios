a42a04962efbbaa5fb91371bae821f5f37ecaef4
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-07-05 15:34:11 -0300

    Implements FileManager Simplenote Extension

---
+++ Simplenote/Classes/FileManager+Simplenote.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+
+// MARK: - Paths
+//
+extension FileManager {
+
+    /// User's Document Directory
+    ///
+    class var documentsURL: URL {
+        guard let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            fatalError("Cannot Access User Documents Directory")
+        }
+
+        return url
+    }
+
+    /// Writes a given Note to the Documents folder
+    ///
+    class func writeToDocuments(note: Note) -> URL? {
+        guard let payload = note.content else {
+            return nil
+        }
+
+        let filename = String(format: "%@.txt", note.simperiumKey)
+        let targetURL = FileManager.documentsURL.appendingPathComponent(filename)
+
+        guard let error = try? payload.write(to: targetURL, atomically: true, encoding: .utf8) else {
+            return targetURL
+        }
+
+        NSLog("Note Exporter Failure: \(error)")
+        return nil
+    }
+}
