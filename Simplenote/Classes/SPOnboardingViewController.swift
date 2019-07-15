420e728ce156e277c35a21a4452dbea5826a2d79
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-07-15 17:30:35 -0300

    New OnboardinViewController

---
+++ Simplenote/Classes/SPOnboardingViewController.swift
@@ -0,0 +1,51 @@
+import Foundation
+import UIKit
+
+
+// MARK: - SPOnboardingViewController
+//
+class SPOnboardingViewController: UIViewController, SPAuthenticationInterface {
+
+    ///
+    ///
+    var authenticator: SPAuthenticator?
+
+    ///
+    ///
+    var optional = false
+
+    ///
+    ///
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationController()
+    }
+
+    @IBAction func signupWasPressed() {
+        guard let authenticator = authenticator else {
+            fatalError()
+        }
+
+        let viewController = SPSignupViewController(authenticator: authenticator)
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+
+    @IBAction func loginWasPressed() {
+        guard let authenticator = authenticator else {
+            fatalError()
+        }
+
+        let viewController = SPLoginViewController(authenticator: authenticator)
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+}
+
+
+// MARK: - Private
+//
+private extension SPOnboardingViewController {
+
+    func setupNavigationController() {
+        navigationController?.isNavigationBarHidden = true
+    }
+}
