e42290e197a17a884fdd8202a0a0d2a234fef01a
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-07-16 16:51:55 -0300

    Authentication: Styling Navigation

--- Simplenote/Classes/SPLoginViewController.swift
+++ Simplenote/Classes/SPLoginViewController.swift
@@ -10,16 +10,40 @@ class SPLoginViewController: UIViewController {
     ///
     let authenticator: SPAuthenticator
 
+    /// Simperium's Authenticator Instance
     ///
+    let authenticator: SPAuthenticator
+
+    /// NSCodable Required Initializer
     ///
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    ///
+    /// Designated Initializer
     ///
     init(authenticator: SPAuthenticator) {
         self.authenticator = authenticator
         super.init(nibName: nil, bundle: nil)
+    }
+
+
+    // MARK: - Overridden Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationController()
+    }
+}
+
+
+// MARK: - Private
+//
+private extension SPLoginViewController {
+
+    func setupNavigationController() {
+        title = NSLocalizedString("Log In", comment: "Log In Title")
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.applySimplenoteLightStyle()
     }
 }
--- Simplenote/Classes/SPSignupViewController.swift
+++ Simplenote/Classes/SPSignupViewController.swift
@@ -6,20 +6,39 @@ import UIKit
 //
 class SPSignupViewController: UIViewController {
 
-    ///
+    /// Simperium's Authenticator Instance
     ///
     let authenticator: SPAuthenticator
 
-    ///
+    /// NSCodable Required Initializer
     ///
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    ///
+    /// Designated Initializer
     ///
     init(authenticator: SPAuthenticator) {
         self.authenticator = authenticator
         super.init(nibName: nil, bundle: nil)
+    }
+
+    // MARK: - Overridden Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationController()
+    }
+}
+
+
+// MARK: - Private
+//
+private extension SPSignupViewController {
+
+    func setupNavigationController() {
+        title = NSLocalizedString("Sign Up", comment: "Sign Up Title")
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.applySimplenoteStyle()
     }
 }
