bc197ca82b91a5e5c08c7b309d9283830a392158
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-07-17 13:05:58 -0300

    New Colors

--- Simplenote/Classes/UIColor+Simplenote.swift
+++ Simplenote/Classes/UIColor+Simplenote.swift
@@ -7,6 +7,21 @@ import UIKit
 extension UIColor {
 
     @objc
+    static func simplenoteAlmostBlack() -> UIColor {
+        return UIColor(red: 18.0 / 255.0, green: 23.0 / 255.0, blue: 26.0 / 255.0, alpha: 1.0)
+    }
+
+    @objc
+    static func simplenoteMidBlue() -> UIColor {
+        return UIColor(red: 33.0 / 255.0, green: 112.0 / 255.0, blue: 176.0 / 255.0, alpha: 1.0)
+    }
+
+    @objc
+    static func simplenoteLightNavy() -> UIColor {
+        return UIColor(red: 19.0 / 255.0, green: 93.0 / 255.0, blue: 149.0 / 255.0, alpha: 1.0)
+    }
+
+    @objc
     static func simplenoteGreen() -> UIColor {
         return UIColor(red: 29.0/255.0, green: 120.0/255.0, blue: 25.0/255.0, alpha: 1.0)
     }
