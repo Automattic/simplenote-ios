28c293fed488c8d84f6a7a23c80d53b09385dd8c
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-07-18 00:03:21 -0300

    New UIColor(s)

--- Simplenote/Classes/UIColor+Simplenote.swift
+++ Simplenote/Classes/UIColor+Simplenote.swift
@@ -12,31 +12,6 @@ extension UIColor {
     }
 
     @objc
-    static func simplenoteMidBlue() -> UIColor {
-        return UIColor(red: 33.0 / 255.0, green: 112.0 / 255.0, blue: 176.0 / 255.0, alpha: 1.0)
-    }
-
-    @objc
-    static func simplenoteLightNavy() -> UIColor {
-        return UIColor(red: 19.0 / 255.0, green: 93.0 / 255.0, blue: 149.0 / 255.0, alpha: 1.0)
-    }
-
-    @objc
-    static func simplenoteGreen() -> UIColor {
-        return UIColor(red: 29.0/255.0, green: 120.0/255.0, blue: 25.0/255.0, alpha: 1.0)
-    }
-
-    @objc
-    static func simplenoteRed() -> UIColor {
-        return UIColor(red: 202.0/255.0, green: 38.0/255.0, blue: 34.0/255.0, alpha: 1.0)
-    }
-
-    @objc
-    static func simplenoteYellow() -> UIColor {
-        return UIColor(red: 214.0/255.0, green: 176.0/255.0, blue: 44.0/255.0, alpha: 1.0)
-    }
-
-    @objc
     static func simplenoteBlue() -> UIColor {
         return UIColor(red: 72.0/255.0, green: 149.0/255.0, blue: 217.0/255.0, alpha: 1.0)
     }
@@ -49,5 +24,40 @@ extension UIColor {
     @objc
     static func simplenoteGray500() -> UIColor {
         return UIColor(red: 137.0/255.0, green: 145.0/255.0, blue: 153.0/255.0, alpha: 1.0)
+    }
+
+    @objc
+    static func simplenoteGreen() -> UIColor {
+        return UIColor(red: 29.0/255.0, green: 120.0/255.0, blue: 25.0/255.0, alpha: 1.0)
+    }
+
+    @objc
+    static func simplenoteLightNavy() -> UIColor {
+        return UIColor(red: 19.0 / 255.0, green: 93.0 / 255.0, blue: 149.0 / 255.0, alpha: 1.0)
+    }
+
+    @objc
+    static func simplenoteLightPink() -> UIColor {
+        return UIColor(red: 227.0 / 255.0, green: 224.0 / 255.0, blue: 226.0 / 255.0, alpha: 1.0)
+    }
+
+    @objc
+    static func simplenoteMidBlue() -> UIColor {
+        return UIColor(red: 33.0 / 255.0, green: 112.0 / 255.0, blue: 176.0 / 255.0, alpha: 1.0)
+    }
+
+    @objc
+    static func simplenotePalePurple() -> UIColor {
+        return UIColor(red: 182.0 / 255.0, green: 178.0 / 255.0, blue: 184.0 / 255.0, alpha: 1.0)
+    }
+
+    @objc
+    static func simplenoteRed() -> UIColor {
+        return UIColor(red: 202.0/255.0, green: 38.0/255.0, blue: 34.0/255.0, alpha: 1.0)
+    }
+
+    @objc
+    static func simplenoteYellow() -> UIColor {
+        return UIColor(red: 214.0/255.0, green: 176.0/255.0, blue: 44.0/255.0, alpha: 1.0)
     }
 }
