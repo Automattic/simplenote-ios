ab4a6fe06c7786e42510ec91df0faa7df2caaead
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-07-18 01:39:37 -0300

    New Assets

--- Simplenote/Classes/UIImage+Simplenote.swift
+++ Simplenote/Classes/UIImage+Simplenote.swift
@@ -10,7 +10,7 @@ extension UIImage {
     ///
     @objc
     static var onePasswordImage: UIImage {
-        return UIImage(named: "onepassword_button")!
+        return UIImage(named: "button_onepassword")!
     }
 
     /// Returns the Pinned Icon, to be used by the Notes List
@@ -25,5 +25,19 @@ extension UIImage {
     @objc
     static var sharedImage: UIImage {
         return UIImage(named: "icon_shared")!
+    }
+
+    /// Returns the Visibility On Image
+    ///
+    @objc
+    static var visibilityOnImage: UIImage {
+        return UIImage(named: "button_visibility_on")!
+    }
+
+    /// Returns the Visibility Off Image
+    ///
+    @objc
+    static var visibilityOffImage: UIImage {
+        return UIImage(named: "button_visibility_off")!
     }
 }
