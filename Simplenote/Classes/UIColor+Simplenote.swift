56cb94b784c07762506a9631bd90b10e6719d191
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-07-23 15:35:02 -0300

    New Named UIColor Mechanism

--- Simplenote/Classes/UIColor+Simplenote.swift
+++ Simplenote/Classes/UIColor+Simplenote.swift
@@ -4,35 +4,48 @@ import UIKit
 
 // MARK: - Simplenote UIColor(s)
 //
+@objc
+enum UIColorName: Int {
+    case destructiveActionColor
+    case secondaryActionColor
+    case tertiaryActionColor
+}
+
+extension UIColorName {
+    var assetCatalogName: String {
+        switch self {
+        default:
+            return ""
+        }
+    }
+
+    var legacyThemeKey: ThemeKey {
+        switch self {
+        case .destructiveActionColor:
+            return .destructiveActionColor
+        case .secondaryActionColor:
+            return .secondaryActionColor
+        case .tertiaryActionColor:
+            return .tertiaryActionColor
+        }
+    }
+}
+
 extension UIColor {
-
     @objc
-    static func simplenoteGreen() -> UIColor {
-        return UIColor(red: 29.0/255.0, green: 120.0/255.0, blue: 25.0/255.0, alpha: 1.0)
+    static func color(name: UIColorName) -> UIColor? {
+        if #available(iOS 13.0, *) {
+            return UIColor(named: name.assetCatalogName)
+        }
+
+        return theme.color(forKey: name.legacyThemeKey)
     }
+}
 
-    @objc
-    static func simplenoteRed() -> UIColor {
-        return UIColor(red: 202.0/255.0, green: 38.0/255.0, blue: 34.0/255.0, alpha: 1.0)
-    }
 
-    @objc
-    static func simplenoteYellow() -> UIColor {
-        return UIColor(red: 214.0/255.0, green: 176.0/255.0, blue: 44.0/255.0, alpha: 1.0)
-    }
+private extension UIColor {
 
-    @objc
-    static func simplenoteBlue() -> UIColor {
-        return UIColor(red: 72.0/255.0, green: 149.0/255.0, blue: 217.0/255.0, alpha: 1.0)
-    }
-
-    @objc
-    static func simplenoteGray600() -> UIColor {
-        return UIColor(red: 111.0/255.0, green: 119.0/255.0, blue: 128.0/255.0, alpha: 1.0)
-    }
-
-    @objc
-    static func simplenoteGray500() -> UIColor {
-        return UIColor(red: 137.0/255.0, green: 145.0/255.0, blue: 153.0/255.0, alpha: 1.0)
+    static var theme: VSTheme {
+        return VSThemeManager.shared().theme()
     }
 }
