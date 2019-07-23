3d8329e31d5ecf9d5f40c6fa67e794335248f75c
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-07-23 15:56:47 -0300

    Promoting UIColorName to its own file

--- Simplenote/Classes/UIColor+Simplenote.swift
+++ Simplenote/Classes/UIColor+Simplenote.swift
@@ -4,34 +4,10 @@ import UIKit
 
 // MARK: - Simplenote UIColor(s)
 //
-@objc
-enum UIColorName: Int {
-    case destructiveActionColor
-    case secondaryActionColor
-    case tertiaryActionColor
-}
-
-extension UIColorName {
-    var assetCatalogName: String {
-        switch self {
-        default:
-            return ""
-        }
-    }
-
-    var legacyThemeKey: ThemeKey {
-        switch self {
-        case .destructiveActionColor:
-            return .destructiveActionColor
-        case .secondaryActionColor:
-            return .secondaryActionColor
-        case .tertiaryActionColor:
-            return .tertiaryActionColor
-        }
-    }
-}
-
 extension UIColor {
+
+    /// Returns the UIColor instance matching a given UIColorName. If any
+    ///
     @objc
     static func color(name: UIColorName) -> UIColor? {
         if #available(iOS 13.0, *) {
@@ -43,6 +19,8 @@ extension UIColor {
 }
 
 
+// MARK: - Private
+//
 private extension UIColor {
 
     static var theme: VSTheme {
