e9654797dc2d102785cf25e477f2b400cb1a0304
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-07-24 16:58:17 -0300

    New UIColorName Mechanism

--- Simplenote/VSTheme+Keys.swift
+++ Simplenote/VSTheme+Keys.swift
@@ -4,17 +4,58 @@ import Foundation
 // MARK: - ThemeKey represents all of the available Keys for the current theme.
 //
 enum ThemeKey: String {
-    case backgroundColor
     case destructiveActionColor
     case secondaryActionColor
     case tertiaryActionColor
+    case actionSheetBackgroundColor
+    case actionSheetButtonFontColor
+    case actionSheetButtonBackgroundHighlightColor
+    case actionSheetDividerColor
+    case actionSheetFontColor
+    case actionViewButtonDisabledColor
+    case actionViewStatusFontColor
+    case actionViewToggleTintColor
+    case backgroundColor
+    case barTintColor
+    case collaboratorCellPrimaryLabelFontColor
+    case collaboratorCellSecondaryLabelFontColor
+    case collaboratorTextFieldTextColor
+    case collaboratorTextFieldPlaceholderTextColor
+    case emptyListViewFontColor
+    case horizontalPickerBorderColor
+    case horizontalPickerTitleFontColor
+    case lockBackgroundColor
+    case lockTextColor
+    case navigationBarTitleFontColor
     case noteBodyFontPreviewColor
-    case noteBodyLineHeightPercentage
     case noteCellBackgroundSelectionColor
     case noteHeadlineFontColor
-    case tableViewTextLabelColor
+    case searchBarImageColor
+    case searchBarFontColor
+    case searchHighlightFontColor
+    case switchTintColor
+    case switchOnTintColor
+    case tableViewBackgroundColor
+    case tableViewCellBackgroundHighlightColor
     case tableViewDetailTextLabelColor
+    case tableViewSeparatorColor
+    case tableViewTextLabelColor
+    case tagListFontColor
+    case tagListFontHighlightColor
+    case tagListHighlightBackgroundColor
+    case tagListSeparatorColor
+    case tagViewAutoCompleteFontColor
+    case tagViewFontColor
+    case tagViewFontColorSelected
+    case tagViewFontHighlightedColor
+    case tagViewPlaceholderColor
+    case tagViewDeletionBackgroundColor
+    case tagViewDeletionBackgroundBorderColor
+    case textColor
     case tintColor
+    case versionPickerDateFontColor
+
+    case noteBodyLineHeightPercentage
 }
 
 
