4f46d5e7059fe648fd83615c083813c867710f27
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-07-30 10:43:52 -0300

    Fixing navigationBar issues in iOS 13

--- Simplenote/DB5/VSThemeManager.m
+++ Simplenote/DB5/VSThemeManager.m
@@ -106,27 +106,46 @@ NSString *const VSThemeManagerThemePrefKey = @"VSThemeManagerThemePrefKey";
 
 - (void)applyAppearanceStylingForTheme:(VSTheme *)theme {
 
-    UIFont *barButtonFont = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
-    [[UIBarButtonItem appearance] setTitleTextAttributes:@{NSFontAttributeName:barButtonFont}
-                                                forState:UIControlStateNormal];
-    
-    UIFont *navigationBarTitleFont = [UIFont systemFontOfSize:17 weight:UIFontWeightSemibold];
-    UIColor *navigationBarTitleColor = [UIColor colorWithName:UIColorNameNavigationBarTitleFontColor];
+    /// Style: BarButtonItem
+    ///
+    NSDictionary *barButtonTitleAttributes = @{
+        NSFontAttributeName: [UIFont preferredFontForTextStyle:UIFontTextStyleBody]
+    };
+
+    [[UIBarButtonItem appearance] setTitleTextAttributes:barButtonTitleAttributes forState:UIControlStateNormal];
+
+    /// Style: NavigationBar
+    ///
     UIColor *barTintColor = [UIColor colorWithName:UIColorNameBackgroundColor];
     UIImage *navbarShadowImage = [UIImage imageWithName:UIImageNameNavigationBarShadowImage];
     UIImage *navbarBackgroundImage = [UIImage imageWithName:UIImageNameNavigationBarBackgroundImage];
     UIImage *navbarPromptImage = [UIImage imageWithName:UIImageNameNavigationBarBackgroundPromptImage];
 
-    [[UINavigationBar appearanceWhenContainedInInstancesOfClasses:@[[SPNavigationController class]]] setTitleTextAttributes:@{NSFontAttributeName: navigationBarTitleFont,
-                                                           NSForegroundColorAttributeName: navigationBarTitleColor}];
-    [[UINavigationBar appearanceWhenContainedInInstancesOfClasses:@[[SPNavigationController class]]] setBarTintColor:barTintColor];
-    [[UINavigationBar appearanceWhenContainedInInstancesOfClasses:@[[SPNavigationController class]]] setShadowImage:[navbarShadowImage resizableImageWithCapInsets:UIEdgeInsetsMake(1, 0, 0, 0) resizingMode:UIImageResizingModeTile]];
-    [[UINavigationBar appearanceWhenContainedInInstancesOfClasses:@[[SPNavigationController class]]] setBackgroundImage:[navbarBackgroundImage
-                                                      resizableImageWithCapInsets:UIEdgeInsetsMake(44, 0, 0, 0)]
-                                       forBarMetrics:UIBarMetricsDefault];
-    [[UINavigationBar appearanceWhenContainedInInstancesOfClasses:@[[SPNavigationController class]]] setBackgroundImage:[navbarPromptImage
-                                                      resizableImageWithCapInsets:UIEdgeInsetsMake(64, 0, 0, 0)]
-                                       forBarMetrics:UIBarMetricsDefaultPrompt];
+    if (@available(iOS 13, *)) {
+        // WHY we're disabling the `resizableImage:` for iOS +13:
+        //
+        //  1.  It neutralizes iOS 13's "effective asset" resolution capabilities. The UI ends up stuck with the incorrect image.
+        //  2.  Tested in an array of devices. iOS *is* stretching the image automatically, anyways
+        //  3.  The asset itself is a line of 1x44pt. No curved areas, it's okay to stretch without considering insets.
+        //
+        //  *Plus:* Yes, we'd much rather having @available == false. BUT that triggers an Xcode warning!
+        //
+    } else {
+        navbarBackgroundImage = [navbarBackgroundImage resizableImageWithCapInsets:UIEdgeInsetsMake(44, 0, 0, 0)];
+        navbarPromptImage = [navbarPromptImage resizableImageWithCapInsets:UIEdgeInsetsMake(64, 0, 0, 0)];
+    }
+
+    NSDictionary *barTitleAttributes = @{
+        NSFontAttributeName: [UIFont systemFontOfSize:17 weight:UIFontWeightSemibold],
+        NSForegroundColorAttributeName: [UIColor colorWithName:UIColorNameNavigationBarTitleFontColor]
+    };
+
+    id barAppearance = [UINavigationBar appearanceWhenContainedInInstancesOfClasses:@[[SPNavigationController class]]];
+    [barAppearance setBarTintColor:barTintColor];
+    [barAppearance setBackgroundImage:navbarBackgroundImage forBarMetrics:UIBarMetricsDefault];
+    [barAppearance setBackgroundImage:navbarPromptImage forBarMetrics:UIBarMetricsDefaultPrompt];
+    [barAppearance setShadowImage:navbarShadowImage];
+    [barAppearance setTitleTextAttributes:barTitleAttributes];
 }
 
 @end
