894c334e10e9b3fb1665bba6d59103ecb928e12a
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-08-01 14:36:52 -0300

    Implements UserDefaults+Tests

---
+++ SimplenoteTests/UserDefaults+Tests.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+
+// MARK: - UserDefaults Extension Methods
+//
+extension UserDefaults {
+
+    /// Resets all of the receiver's values
+    ///
+    func reset() {
+        for (key, _) in dictionaryRepresentation() {
+            removeObject(forKey: key)
+        }
+
+        synchronize()
+    }
+}
