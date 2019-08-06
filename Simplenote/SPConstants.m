d17d51adca90829e944aa40bb229e0eb0705716e
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-08-06 12:35:10 -0300

    Terms of Service should be https

--- Simplenote/SPConstants.m
+++ Simplenote/SPConstants.m
@@ -16,7 +16,7 @@
 
 NSString *const kSimperiumBaseURL                   = @"https://api.simperium.com/1";
 NSString *const kSimperiumForgotPasswordURL         = @"https://app.simplenote.com/forgot/";
-NSString *const kSimperiumTermsOfServiceURL         = @"http://simplenote.com/terms/";
+NSString *const kSimperiumTermsOfServiceURL         = @"https://simplenote.com/terms/";
 NSString *const kSimperiumPreferencesObjectKey      = @"preferences-key";
 
 NSString *const kAutomatticAnalyticLearnMoreURL     = @"https://automattic.com/cookies";
