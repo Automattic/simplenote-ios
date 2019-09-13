6b4743d686d8131af7fbaa943f555fca02015e52
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-09-13 12:50:23 -0400

    Implements UIBezierPath+Simplenote

---
+++ Simplenote/Classes/UIBezierPath+Simplenote.swift
@@ -0,0 +1,19 @@
+import Foundation
+import UIKit
+
+
+// MARK: - UIBezierPath Methods
+//
+extension UIBezierPath {
+
+    /// Returns an UIImage representation of the receiver.
+    ///
+    @objc
+    func imageRepresentation(color: UIColor) -> UIImage {
+        return UIGraphicsImageRenderer(size: bounds.size).image { [unowned self] context in
+            self.addClip()
+            color.setFill()
+            context.fill(bounds)
+        }
+    }
+}
