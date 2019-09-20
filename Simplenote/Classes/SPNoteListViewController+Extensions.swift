a75ae0d2bbb9b467d4dc6a421f91d6efdf1681e0
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-09-20 18:11:30 -0300

    SPNoteEditorViewController: Fixing Search in Peek&Pop Scenarios

--- Simplenote/Classes/SPNoteListViewController+Extensions.swift
+++ Simplenote/Classes/SPNoteListViewController+Extensions.swift
@@ -28,6 +28,7 @@ extension SPNoteListViewController: UIViewControllerPreviewingDelegate {
         let editorViewController = SPAppDelegate.shared().noteEditorViewController
         editorViewController.update(note)
         editorViewController.isPreviewing = true
+        editorViewController.searchString = searchText
 
         return editorViewController
     }
