403d154f13feb36392fbcbff23821dd3af6657b2
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-09-20 19:29:45 -0300

    UIColor+Helpers: New dynamicImageRepresentation API

--- Simplenote/Classes/UIColor+Helpers.swift
+++ Simplenote/Classes/UIColor+Helpers.swift
@@ -6,9 +6,24 @@ import UIKit
 //
 extension UIColor {
 
-    /// Returns an UIImage representation of the receiver, with the specified size.
+    /// Returns an UIImage representation of the receiver, with the specified size, and Dark Mode support.
     ///
-    func imageRepresentation(size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
+    func dynamicImageRepresentation(size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
+        guard #available(iOS 13.0, *) else {
+            return imageRepresentation(size: size)
+        }
+
+        let darkImage = resolvedColor(with: .purelyDarkTraits).imageRepresentation(size: size)
+        let lightImage = resolvedColor(with: .purelyLightTraits).imageRepresentation(size: size)
+
+        lightImage.imageAsset?.register(darkImage, with: .purelyDarkTraits)
+
+        return lightImage
+    }
+
+    /// Returns a rastrerized image of the specified size, representing the receiver instance.
+    ///
+    private func imageRepresentation(size: CGSize) -> UIImage {
         let rect = CGRect(origin: .zero, size: size)
 
         return UIGraphicsImageRenderer(size: size).image { context in
