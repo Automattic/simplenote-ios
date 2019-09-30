17743be814dcc93a85462314ca3db2272be5f532
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-09-30 11:36:43 -0300

    SPNoteListViewController: Adjusting searchBar appearance

--- Simplenote/Classes/SPNoteListViewController+Extensions.swift
+++ Simplenote/Classes/SPNoteListViewController+Extensions.swift
@@ -16,11 +16,15 @@ extension SPNoteListViewController {
     /// Refreshes the SearchBar Style.
     ///
     @objc
-    func styleSearchBar() {
+    func styleSearchBar(_ searchBar: UISearchBar) {
+        let backgroundImage = UIImage()
+        let backgroundColor = UIColor.color(name: .backgroundColor)?.withAlphaComponent(Constants.searchBarBackgroundAlpha)
         let searchIconColor = UIColor.color(name: .simplenoteSlateGrey)
         let searchIconImage = UIImage.image(name: .searchIconImage)?.withOverlayColor(searchIconColor)
 
+        searchBar.backgroundColor = backgroundColor
         searchBar.setImage(searchIconImage, for: .search, state: .normal)
+        searchBar.setBackgroundImage(backgroundImage, for: .any, barMetrics: .default)
         searchBar.setSearchFieldBackgroundImage(.searchBarBackgroundImage, for: .normal)
         searchBarContainer.backgroundColor = .clear
 
@@ -67,4 +71,14 @@ extension SPNoteListViewController: UIViewControllerPreviewingDelegate {
         editorViewController.isPreviewing = false
         navigationController?.pushViewController(editorViewController, animated: true)
     }
+}
+
+
+// MARK: - Constants
+//
+private enum Constants {
+
+    /// SearchBar's Background Alpha, so that it matches with the navigationBar!
+    ///
+    static let searchBarBackgroundAlpha = CGFloat(0.9)
 }
