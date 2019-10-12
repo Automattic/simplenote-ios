d8de7572432e14d01fcf76f0b604202b443d7025
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-10-12 00:08:54 -0300

    Implements NSPredicate+Simplenote

---
+++ Simplenote/Classes/NSPredicate+Simplenote.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+
+// MARK: - NSPredicate Methods
+//
+extension NSPredicate {
+
+    /// Returns a NSPredicate that will match:
+    ///
+    ///     A. Empty JSON Arrays (with random padding)
+    ///     B. Empty Strings
+    ///
+    @objc
+    static func predicateForUntaggedNotes() -> NSPredicate {
+        // We'll need to match `Tags` with the following RegEx:
+        //  Empty String  (OR)  Spaces* + [ + Spaces* + ] + Spaces*
+        // Why: `Tags` contains JSON Encoded arrays.
+        let regex = "^()|(null)|(\\s*\\[\\s*]\\s*)$"
+        return NSPredicate(format: "tags MATCHES[n] %@", regex)
+    }
+
+    /// Returns a NSPredicate that will match a given Tag
+    ///
+    @objc
+    static func predicateForTag(with name: String) -> NSPredicate {
+        let filtered = name.replacingOccurrences(of: "\\", with: "\\\\").replacingOccurrences(of: "/", with: "\\/")
+
+        // Individual tags are surrounded by quotes, thus adding quotes to the selected tag ensures only the
+        // correct notes are shown
+        let format = String(format: "\"%@\"", filtered)
+        return NSPredicate(format: "tags CONTAINS[c] %@", format)
+    }
+}
