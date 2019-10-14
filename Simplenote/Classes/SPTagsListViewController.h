761ecb43feb0110951dc84206f48a48c4552abb6
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-10-14 10:30:00 -0300

    SPTagsListViewController: Updates base class

--- Simplenote/Classes/SPTagsListViewController.h
+++ Simplenote/Classes/SPTagsListViewController.h
@@ -1,7 +1,6 @@
 #import <UIKit/UIKit.h>
-#import "SPSidebarViewController.h"
 
 
-@interface SPTagsListViewController : SPSidebarViewController
+@interface SPTagsListViewController : UIViewController
 
 @end
