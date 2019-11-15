595e29fb373541da74f86373257e705bb7b3f1a2
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-11-15 18:27:27 -0300

    UIView+Animations: New APIs

--- Simplenote/Classes/UIView+Animations.swift
+++ Simplenote/Classes/UIView+Animations.swift
@@ -8,6 +8,7 @@ extension UIView {
 
     /// Animates a visibility switch, when applicable
     ///
+    @objc
     func animateVisibility(isHidden: Bool, duration: TimeInterval = 0.3) {
         guard self.isHidden != isHidden else {
             return
@@ -16,5 +17,26 @@ extension UIView {
         UIView.animate(withDuration: duration) {
             self.isHidden = isHidden
         }
+    }
+
+    /// Fades In the receiver
+    ///
+    @objc
+    func fadeIn() {
+        self.alpha = UIKitConstants.alphaZero
+        UIView.animate(withDuration: UIKitConstants.animationQuickDuration) {
+            self.alpha = UIKitConstants.alphaFull
+        }
+    }
+
+    /// Fades Out the receiver
+    ///
+    @objc
+    func fadeOut(completion: @escaping () -> Void) {
+        UIView.animate(withDuration: UIKitConstants.animationQuickDuration, animations: {
+            self.alpha = UIKitConstants.alphaZero
+        }, completion: { _ in
+            completion()
+        })
     }
 }
