fac313d01ecbabf768ca76d342da0e660ccac2c4
Jorge Leandro Perez <jorge.perez@automattic.com>
2019-11-20 18:40:16 -0300

    SPSearchResultsViewController: Initial Implementation

--- Simplenote/Classes/SPSearchResultsViewController.swift
+++ Simplenote/Classes/SPSearchResultsViewController.swift
@@ -6,4 +6,72 @@ import UIKit
 //
 class SPSearchResultsViewController: UIViewController {
 
+    /// Results TableView
+    ///
+    @IBOutlet weak var tableView: UITableView!
+
+
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTableView()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        refreshStyle()
+    }
+}
+
+
+// MARK: - Interface Initialization
+//
+private extension SPSearchResultsViewController {
+
+    /// Sets up the TableView
+    ///
+    func configureTableView() {
+        tableView.register(SPNoteTableViewCell.loadNib(), forCellReuseIdentifier: SPNoteTableViewCell.reuseIdentifier)
+        tableView.tableFooterView = UIView()
+    }
+
+    /// Refreshes the UI Style (iOS <13 DarkMode Support)
+    ///
+    func refreshStyle() {
+        // Refresh the Container's UI
+        view.backgroundColor = .simplenoteBackgroundColor
+
+        // Refresh the Table's UI
+        tableView.applySimplenotePlainStyle()
+        tableView.reloadData()
+    }
+}
+
+
+// MARK: - UITableViewDataSource Methods
+//
+extension SPSearchResultsViewController: UITableViewDataSource {
+
+    // TODO: Demo code. Replace with the actual CoreData fetch!
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SPNoteTableViewCell.reuseIdentifier, for: indexPath) as? SPNoteTableViewCell else {
+            fatalError()
+        }
+
+        cell.titleText = "Some Title"
+        cell.bodyText = "Body Here!"
+        cell.backgroundColor = .white
+
+        return cell
+    }
 }
