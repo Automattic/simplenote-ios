4606af819c9b974880417a3dbb1cb88c22bc177a
Charlie <scheersound@gmail.com>
2020-01-14 11:28:57 +0900

    Remove copyright information from SPSimpleTextPrintFormatter

--- Simplenote/SPSimpleTextPrintFormatter.swift
+++ Simplenote/SPSimpleTextPrintFormatter.swift
@@ -1,17 +1,11 @@
-//
-//  SimplenoteSimpleTextPrintFormatter.swift
-//  Simplenote
-//
-//  Created by Charlie Scheer on 1/10/20.
-//  Copyright © 2020 Automattic. All rights reserved.
-//
-
 import UIKit
 
 class SPSimpleTextPrintFormatter: UISimpleTextPrintFormatter {
     override init(text: String) {
         super.init(text: text)
         
+        //Check for darkmode
+        //If dark mode is enabled change the text color to black before printing
         if #available(iOS 12.0, *) {
             if SPUserInterface.isDark {
                 self.color = UIColor.black
