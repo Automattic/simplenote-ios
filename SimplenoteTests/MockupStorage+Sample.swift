4d0aae83b228fec287a6327614907aef2d8edd94
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-01-16 11:33:37 -0300

    MockupStorage: Updates Public API

--- SimplenoteTests/MockupStorage+Sample.swift
+++ SimplenoteTests/MockupStorage+Sample.swift
@@ -9,13 +9,14 @@ extension MockupStorageManager {
     /// Inserts a new (Sample) Note into the receiver's Main MOC
     ///
     @discardableResult
-    func insertSampleNote() -> Note {
+    func insertSampleNote(contents: String = "") -> Note {
         guard let note = NSEntityDescription.insertNewObject(forEntityName: Note.entityName, into: viewContext) as? Note else {
             fatalError()
         }
 
         note.modificationDate = Date()
         note.creationDate = Date()
+        note.content = contents
 
         return note
     }
@@ -23,10 +24,12 @@ extension MockupStorageManager {
     /// Inserts a new (Sample) Tag into the receiver's Main MOC
     ///
     @discardableResult
-    func insertSampleTag() -> Tag {
+    func insertSampleTag(name: String = "") -> Tag {
         guard let tag = NSEntityDescription.insertNewObject(forEntityName: Tag.entityName, into: viewContext) as? Tag else {
             fatalError()
         }
+
+        tag.name = name
 
         return tag
     }
