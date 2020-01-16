236b1732daea1359b948c90f604bbb668378374e
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-01-16 20:03:22 -0300

    Adds Asset: .sortOrder

--- Simplenote/Classes/UIImageName.swift
+++ Simplenote/Classes/UIImageName.swift
@@ -32,6 +32,7 @@ enum UIImageName: Int, CaseIterable {
     case settings
     case share
     case shared
+    case sortOrder
     case tag
     case tagViewDeletion
     case tags
@@ -104,6 +105,8 @@ extension UIImageName {
             return "icon_share"
         case .shared:
             return "icon_shared"
+        case .sortOrder:
+            return "icon_sort_order"
         case .tag:
             return "icon_tag"
         case .tagViewDeletion:
