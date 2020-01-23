0781ae5eed3128df220ad9e2aff04efb6846c763
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-01-23 10:16:23 -0300

    String+Simplenote: Localizing Search Operator

--- Simplenote/Classes/String+Simplenote.swift
+++ Simplenote/Classes/String+Simplenote.swift
@@ -7,7 +7,7 @@ extension String {
 
     /// Returns the Search Operator we should recognize, when filtering out entities with a given Tag
     ///
-    static let searchOperatorForTags = "tag:"
+    static let searchOperatorForTags = NSLocalizedString("tag:", comment: "Search Operator for tags. Please preserve the semicolons when translating!")
 
 
     /// Returns the Suffix string after a given `prefix` (if any!)
