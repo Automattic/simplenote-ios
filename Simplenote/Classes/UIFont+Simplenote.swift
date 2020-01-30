dbe54765ae4129c668de02f2b8a31a52d1c0c64b
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-01-30 11:55:28 -0300

    UIFont+Simplenote: New API

--- Simplenote/Classes/UIFont+Simplenote.swift
+++ Simplenote/Classes/UIFont+Simplenote.swift
@@ -6,6 +6,10 @@ import UIKit
 //
 extension UIFont {
 
+    /// Default Asset Height Multiplier
+    ///
+    static let defaultInlineAssetSizeMultiplier = CGFloat(0.7)
+
     /// Returns the System Font for a given Style and Weight
     ///
     static func preferredFont(for style: TextStyle, weight: Weight) -> UIFont {
@@ -13,5 +17,11 @@ extension UIFont {
         let font = UIFont.systemFont(ofSize: descriptor.pointSize, weight: weight)
 
         return UIFontMetrics(forTextStyle: style).scaledFont(for: font)
+    }
+
+    /// Returns the (Expected) InlineAsset Height: We consider the lineHeight, and apply a (default) multiplier, to account for ascending and descending metrics.
+    ///
+    func inlineAssetHeight(multiplier: CGFloat = defaultInlineAssetSizeMultiplier) -> CGFloat {
+        return ceil(lineHeight * multiplier)
     }
 }
