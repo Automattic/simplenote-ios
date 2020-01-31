11c658b7d53869f4a9fe797d3560918e937e8260
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-01-31 10:53:18 -0300

    UIFont+Simplenote: Implements internal cache

--- Simplenote/Classes/UIFont+Simplenote.swift
+++ Simplenote/Classes/UIFont+Simplenote.swift
@@ -9,9 +9,46 @@ extension UIFont {
     /// Returns the System Font for a given Style and Weight
     ///
     static func preferredFont(for style: TextStyle, weight: Weight) -> UIFont {
-        let descriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
-        let font = UIFont.systemFont(ofSize: descriptor.pointSize, weight: weight)
+        if let cachedFont = FontCache.cachedFont(for: style, weight: weight) {
+            return cachedFont
+        }
 
-        return UIFontMetrics(forTextStyle: style).scaledFont(for: font)
+        let descriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
+        let unstyledFont = UIFont.systemFont(ofSize: descriptor.pointSize, weight: weight)
+        let preferredFont = UIFontMetrics(forTextStyle: style).scaledFont(for: unstyledFont)
+
+        FontCache.storeFont(preferredFont, style: style, weight: weight)
+
+        return preferredFont
+    }
+}
+
+
+// MARK: - FontCache: Performance Helper!
+//
+private enum FontCache {
+
+    /// Internal Cache
+    ///
+    private static var cache = [UIFont.TextStyle: [UIFont.Weight: UIFont]]()
+
+    /// Returns the stored entry for the specified Style + Weight combination (If Any!)
+    /// - Note: This method is, definitely, non threadsafe!
+    ///
+    static func cachedFont(for style: UIFont.TextStyle, weight: UIFont.Weight) -> UIFont? {
+        assert(Thread.isMainThread)
+
+        return cache[style]?[weight]
+    }
+
+    /// Stores a given UIFont instance, under the specified Style and Weight keys
+    /// - Note: This method is, definitely, non threadsafe!
+    ///
+    static func storeFont(_ font: UIFont, style: UIFont.TextStyle, weight: UIFont.Weight) {
+        assert(Thread.isMainThread)
+
+        var updatedStyleMap = cache[style] ?? [:]
+        updatedStyleMap[weight] = font
+        cache[style] = updatedStyleMap
     }
 }
