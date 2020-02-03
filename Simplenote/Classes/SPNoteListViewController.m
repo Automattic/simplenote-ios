1ed61768a27d5e9567017797525fce9c01037f10
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-02-03 17:45:17 -0300

    SPNoteListViewController: Preventing insane bottom insets

--- Simplenote/Classes/SPNoteListViewController.m
+++ Simplenote/Classes/SPNoteListViewController.m
@@ -670,11 +670,11 @@
     _keyboardHeight = MIN(keyboardFrame.size.height, keyboardFrame.size.width);
     
     UIEdgeInsets tableviewInsets = self.tableView.contentInset;
-    tableviewInsets.bottom += _keyboardHeight;
+    tableviewInsets.bottom = _keyboardHeight;
     self.tableView.contentInset = tableviewInsets;
     
     UIEdgeInsets scrollInsets = self.tableView.scrollIndicatorInsets;
-    scrollInsets.bottom += _keyboardHeight;
+    scrollInsets.bottom = _keyboardHeight;
     self.tableView.scrollIndicatorInsets = tableviewInsets;
 }
 
