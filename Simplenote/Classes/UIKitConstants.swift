29b0a2ef1efdf43f8158d53b3b5d52afb3c77b6c
SylvesterWilmott <36532468+SylvesterWilmott@users.noreply.github.com>
2020-02-21 14:10:35 +0000

    UIKitConstants: Add alpha0_8

--- Simplenote/Classes/UIKitConstants.swift
+++ Simplenote/Classes/UIKitConstants.swift
@@ -9,6 +9,7 @@ class UIKitConstants: NSObject {
     static let alpha0_4 = CGFloat(0.4)
     static let alpha0_5 = CGFloat(0.5)
     static let alpha0_6 = CGFloat(0.6)
+    static let alpha0_8 = CGFloat(0.8)
     static let alpha1_0 = CGFloat(1)
     static let animationDelayZero = TimeInterval(0)
     static let animationQuickDuration = TimeInterval(0.1)
