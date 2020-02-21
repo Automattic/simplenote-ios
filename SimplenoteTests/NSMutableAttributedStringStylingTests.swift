5e1fbedc4c289649bbb16ac80718eba8adb86f26
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-02-21 15:18:38 -0300

    SPChecklistTest > NSMutableAttributedStringStylingTests

--- SimplenoteTests/NSMutableAttributedStringStylingTests.swift
+++ SimplenoteTests/NSMutableAttributedStringStylingTests.swift
@@ -1,39 +1,38 @@
-//
-//  SPChecklistTest.swift
-//  SimplenoteTests
-//
-
 import XCTest
+@testable import Simplenote
 
-class SPChecklistTest: XCTestCase {
+
+// MARK: - NSMutableAttributedString Styling Tests
+//
+class NSMutableAttributedStringStylingTests: XCTestCase {
+
+    ///
+    ///
     func testChecklistShouldNotRenderWithinText() {
         let inlineChecklist = "This is a badly formed todo - [ ] Buy avocados"
-        
-        let regex = try! NSRegularExpression(pattern: CheckListRegExPattern, options: NSRegularExpression.Options.anchorsMatchLines)
-        
+        let regex = try! NSRegularExpression(pattern: NSAttributedStringRegexForChecklists, options: .anchorsMatchLines)
         let matches = regex.matches(in: inlineChecklist, options: [], range: NSMakeRange(0, inlineChecklist.count))
         
-        XCTAssertTrue(matches.count == 0)
-    }
-    
-    func testChecklistRenderWithPrefixedWhitespace() {
-        let inlineChecklist = "       - [ ] Buy avocados - [ ] "
-        
-        let regex = try! NSRegularExpression(pattern: CheckListRegExPattern, options: NSRegularExpression.Options.anchorsMatchLines)
-        
-        let matches = regex.matches(in: inlineChecklist, options: [], range: NSMakeRange(0, inlineChecklist.count))
-        
-        XCTAssertTrue(matches.count == 1)
-    }
-    
-    func testMatchProperlyFormattedChecklistSyntax() {
-        let inlineChecklist = "ToDo\n\n- [ ] Buy avocados\n- [ ] Ship it\n- [x ] Malformed!\n- [x] Correct."
-        
-        let regex = try! NSRegularExpression(pattern: CheckListRegExPattern, options: NSRegularExpression.Options.anchorsMatchLines)
-        
-        let matches = regex.matches(in: inlineChecklist, options: [], range: NSMakeRange(0, inlineChecklist.count))
-        
-        XCTAssertTrue(matches.count == 3)
+        XCTAssertTrue(matches.isEmpty)
     }
 
+    ///
+    ///
+    func testChecklistRenderWithPrefixedWhitespace() {
+        let inlineChecklist = "       - [ ] Buy avocados - [ ] "
+        let regex = try! NSRegularExpression(pattern: NSAttributedStringRegexForChecklists, options: .anchorsMatchLines)
+        let matches = regex.matches(in: inlineChecklist, options: [], range: NSMakeRange(0, inlineChecklist.count))
+
+        XCTAssertEqual(matches.count, 1)
+    }
+
+    ///
+    ///
+    func testMatchProperlyFormattedChecklistSyntax() {
+        let inlineChecklist = "ToDo\n\n- [ ] Buy avocados\n- [ ] Ship it\n- [x ] Malformed!\n- [x] Correct."
+        let regex = try! NSRegularExpression(pattern: NSAttributedStringRegexForChecklists, options: .anchorsMatchLines)
+        let matches = regex.matches(in: inlineChecklist, options: [], range: NSMakeRange(0, inlineChecklist.count))
+        
+        XCTAssertEqual(matches.count, 3)
+    }
 }
