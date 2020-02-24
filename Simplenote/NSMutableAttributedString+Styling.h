ca96017d506a66931c19b56c5be920c6933ccf5e
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-02-24 17:26:53 -0300

    NSMutableAttributedString+Styling: Documenting API

--- Simplenote/NSMutableAttributedString+Styling.h
+++ Simplenote/NSMutableAttributedString+Styling.h
@@ -3,9 +3,20 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSMutableAttributedString (Checklists)
+
+/// Replaces Checklist Markers with a SPTextAttachment at the exact same location.
+///
+/// We're expecting a sizingFont because in the NotesList a UILabel is used (which lacks the full TextKit Stack),
+/// which makes it impossible to determine the current Font, from within the NSTextAttachment instance.
+///
+/// @param color: Tinting Color to be applied over the Image
+/// @param sizingFont: Font that should be used to determine the Attachment Size
+/// @param allowsMultiplePerLine: When **YES** we'll support multiple Checklists Image in the same line. Useful for the Notes List.
+///
 - (void)processChecklistsWithColor:(UIColor *)color
                         sizingFont:(UIFont *)sizingFont
              allowsMultiplePerLine:(BOOL)allowsMultiplePerLine;
+
 @end
 
 NS_ASSUME_NONNULL_END
