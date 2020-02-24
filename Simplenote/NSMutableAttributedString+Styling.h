9f466ea029aeaa875174f58874110f894d3d34f6
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-02-24 17:49:46 -0300

    NSMutableAttributedString: Updates comment

--- Simplenote/NSMutableAttributedString+Styling.h
+++ Simplenote/NSMutableAttributedString+Styling.h
@@ -6,12 +6,19 @@ NS_ASSUME_NONNULL_BEGIN
 
 /// Replaces Checklist Markers with a SPTextAttachment at the exact same location.
 ///
-/// We're expecting a sizingFont because in the NotesList a UILabel is used (which lacks the full TextKit Stack),
-/// which makes it impossible to determine the current Font, from within the NSTextAttachment instance.
-///
 /// @param color: Tinting Color to be applied over the Image
 /// @param sizingFont: Font that should be used to determine the Attachment Size
 /// @param allowsMultiplePerLine: When **YES** we'll support multiple Checklists Image in the same line. Useful for the Notes List.
+///
+/// @Discussion
+/// When Multiple Lines are allowed, we'll prepend a space to any attachment that's not at location Zero. Otherwise notes that look like
+/// the following, will definitely look bad: the Checklist Image would end up by the preceding word, without spacing.
+///
+///  `Word -[ ] Item - [ ] Item - [ ] Item`
+///
+/// @Note
+/// We're expecting a sizingFont because in the NotesList a UILabel is used (which lacks the full TextKit Stack),
+/// which makes it impossible to determine the current Font, from within the NSTextAttachment instance.
 ///
 - (void)processChecklistsWithColor:(UIColor *)color
                         sizingFont:(UIFont *)sizingFont
