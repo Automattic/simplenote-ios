9bcfae2dcb3c6c215efa1dd90670de2397585b05
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-02-24 17:17:52 -0300

    NSRegularExpression: Matching Electron's Checklists RegEx

--- Simplenote/Classes/NSRegularExpression+Simplenote.swift
+++ Simplenote/Classes/NSRegularExpression+Simplenote.swift
@@ -9,13 +9,13 @@ extension NSRegularExpression {
     ///
     @objc
     static let regexForChecklists: NSRegularExpression = {
-        try! NSRegularExpression(pattern: "^\\s*(-[ \t]+\\[[xX\\s]\\])", options: .anchorsMatchLines)
+        try! NSRegularExpression(pattern: "^\\s*(-[ \t]+\\[[xX\\s]?\\])", options: .anchorsMatchLines)
     }()
 
     /// Matches Checklists patterns that can be ANYWHERE in the string, not necessarily at the beginning of the string.
     ///
     @objc
     static let regexForChecklistsEmbeddedAnywhere: NSRegularExpression = {
-        try! NSRegularExpression(pattern: "\\s*(-[ \t]+\\[[xX\\s]\\])", options: .anchorsMatchLines)
+        try! NSRegularExpression(pattern: "\\s*(-[ \t]+\\[[xX\\s]?\\])", options: .anchorsMatchLines)
     }()
 }
