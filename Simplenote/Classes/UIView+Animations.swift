2f169428c462aa25ac777ac1a57a98b17423170d
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-05-04 19:59:08 -0300

    UIView+Animations: Fixes animation when hiding / showing

--- Simplenote/Classes/UIView+Animations.swift
+++ Simplenote/Classes/UIView+Animations.swift
@@ -8,7 +8,6 @@ extension UIView {
 
     /// Animates a visibility switch, when applicable.
     /// - Note: We're animating the Alpha property, and effectively switching the `isHidden` property onCompletion.
-    ///         Whenever the animation is thru, we'll also restore Alpha to full (1.0).
     ///
     func animateVisibility(isHidden: Bool, duration: TimeInterval = UIKitConstants.animationQuickDuration) {
         guard self.isHidden != isHidden else {
@@ -16,13 +15,10 @@ extension UIView {
         }
 
         let animationAlpha = isHidden ? UIKitConstants.alpha0_0 : UIKitConstants.alpha1_0
-        let completionAlpha = UIKitConstants.alpha1_0
 
-        UIView.animate(withDuration: duration, animations: {
-            self.alpha = animationAlpha
-        }, completion: { _ in
+        UIView.animate(withDuration: duration) {
             self.isHidden = isHidden
-            self.alpha = completionAlpha
-        })
+            self.alpha = animationAlpha
+        }
     }
 }
