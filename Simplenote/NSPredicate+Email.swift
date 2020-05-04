7aca8291a7351d938610c7d26bd2b93fc2f57fda
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-05-04 20:13:01 -0300

    Implements NSPredicate+Email

---
+++ Simplenote/NSPredicate+Email.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+
+// MARK: - NSPredicate Validation Methods
+//
+extension NSPredicate {
+
+    /// Returns a NSPredicate capable of validating Email Addressess
+    ///
+    static func predicateForEmailValidation() -> NSPredicate {
+        let regex = ".+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2}[A-Za-z]*"
+        return NSPredicate(format: "SELF MATCHES %@", regex)
+    }
+}
