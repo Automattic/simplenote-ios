9b2fd5ddc067e301f44fb76aba7fe99e9f97c0d9
Evgeny Shurakov <eshurakov@users.noreply.github.com>
2020-07-02 01:10:24 +0200

    Add snapping slider

---
+++ Simplenote/SPSnappingSlider.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+@IBDesignable
+final class SPSnappingSlider: UISlider {
+
+    @IBInspectable
+    var step: Float = 1.0 {
+        didSet {
+            handleValueChange()
+        }
+    }
+
+    var onValueChange: ((Float) -> Void)?
+    private(set) var snappedValue: Float = 0.0 {
+        didSet {
+            if oldValue != snappedValue {
+                feedbackGenerator?.impactOccurred()
+                onValueChange?(snappedValue)
+            }
+        }
+    }
+
+    private var feedbackGenerator: UIImpactFeedbackGenerator?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+}
+
+private extension SPSnappingSlider {
+    func configure() {
+        addTarget(self, action: #selector(handleValueChange), for: .valueChanged)
+        handleValueChange()
+
+        configureImpactGenerator()
+    }
+
+    func configureImpactGenerator() {
+        feedbackGenerator = UIImpactFeedbackGenerator()
+        feedbackGenerator?.prepare()
+    }
+
+    @objc
+    func handleValueChange() {
+        let value = round(self.value / step) * step
+        setValue(value, animated: false)
+        snappedValue = value
+    }
+}
