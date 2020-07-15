d8dfe7fedf8658eee0385d10cbacbb6a27a5c8cc
Evgeny Shurakov <eshurakov@users.noreply.github.com>
2020-07-15 22:34:53 +0200

    Rename card observer to delegate and move to presentation controller

--- Simplenote/SPCardTransitionObserver.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-// MARK: - SPCardTransitionObserver: observe card transitioning
-//
-protocol SPCardTransitionObserver: class {
-    func cardWasSwipedToDismiss(_ viewController: UIViewController)
-}
