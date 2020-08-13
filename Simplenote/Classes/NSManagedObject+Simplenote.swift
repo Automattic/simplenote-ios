27fb49d98181884301450ae806f60b84dd2ae163
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-08-13 00:05:08 -0300

    NSManagedObject+Simplenote: Drops public modifier

--- Simplenote/Classes/NSManagedObject+Simplenote.swift
+++ Simplenote/Classes/NSManagedObject+Simplenote.swift
@@ -11,7 +11,7 @@ extension NSManagedObject {
     ///
     /// Note: entity().name returns nil as per iOS 10, in Unit Testing Targets. Awesome.
     ///
-    public class var entityName: String {
+    class var entityName: String {
         /// Note: As of iOS 12, spawning multiple CoreData Stack instances in Unit Tests may result in Console Errors
         /// ("This class is already claimed by another NSEntityDescription"). This error is triggered by the `entity()` method.
         /// For that reason, we're falling back to `classNameWithoutNamespaces()`, which keeps our console tidy.
