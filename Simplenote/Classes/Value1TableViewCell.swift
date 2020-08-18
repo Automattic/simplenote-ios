10062503e45802a911a736108ad886fe560cd89e
Matt Cheetham <m4ttcheetham+automattic@gmail.com>
2020-08-18 16:50:29 +0100

    Fixes styling of value 1 cell on iOS 12 in dark mode

--- Simplenote/Classes/Value1TableViewCell.swift
+++ Simplenote/Classes/Value1TableViewCell.swift
@@ -5,9 +5,18 @@ class Value1TableViewCell: UITableViewCell {
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: .value1, reuseIdentifier: reuseIdentifier)
+        applyStyles()
     }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+    }
+
+    /// Applies custom theming to the cell
+    func applyStyles() {
+        backgroundColor = .simplenoteTableViewCellBackgroundColor
+        selectedBackgroundView?.backgroundColor = .simplenoteLightBlueColor
+        textLabel?.textColor = .simplenoteTextColor
+        detailTextLabel?.textColor = UIColor.color(name: .tableViewDetailTextLabelColor)
     }
 }
