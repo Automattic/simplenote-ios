3f1f4bbb8c5fc72587a2253ad2ea3aa779fef512
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-09-07 16:18:16 -0300

    Drops legacy email validation

--- Simplenote/Classes/NSString+Metadata.h
+++ Simplenote/Classes/NSString+Metadata.h
@@ -3,8 +3,5 @@
 
 // Removes pin and tags
 - (NSArray *)stringArray;
-// Search for a complete word. Does not match substrings of words. Requires fullWord be present
-// and no surrounding alphanumeric characters.
-- (BOOL)containsEmailAddress;
 
 @end
