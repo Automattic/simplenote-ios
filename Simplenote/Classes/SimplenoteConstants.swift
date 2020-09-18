e21d31d5d9edda491ee87c56f4fef0987f594986
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-09-18 15:31:29 -0300

    SimplenoteConstants: Deduplicates String

--- Simplenote/Classes/SimplenoteConstants.swift
+++ Simplenote/Classes/SimplenoteConstants.swift
@@ -39,7 +39,7 @@ struct SimplenoteKeychain {
     /// Pinlock
     ///
     static let pinlockAccount = "SimplenotePin"
-    static let pinlockService = "SimplenotePin"
+    static let pinlockService = pinlockAccount
 
     /// Timestamp
     ///
