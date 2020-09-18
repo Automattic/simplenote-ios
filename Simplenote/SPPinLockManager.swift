41fddc66c9518de9a453af32825efca5b55c06b9
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-09-18 16:17:36 -0300

    SPPinLockManager: Updates Log

--- Simplenote/SPPinLockManager.swift
+++ Simplenote/SPPinLockManager.swift
@@ -57,7 +57,7 @@ class SPPinLockManager: NSObject {
         do {
             try KeychainPasswordItem.timestamp.savePassword(nowTime)
         } catch {
-            NSLog("Keychain Error: \(error)")
+            NSLog("[Keychain] Error Storing Timestamp: \(error)")
         }
     }
 }
