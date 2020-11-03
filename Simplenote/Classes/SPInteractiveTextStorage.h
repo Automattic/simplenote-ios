cf9f33543f5fe9067a2e9be86713e171328fcc4f
Jorge Leandro Perez <jorge.perez@automattic.com>
2020-11-03 18:35:58 -0300

    TextStorage: Splitting properties

--- Simplenote/Classes/SPInteractiveTextStorage.h
+++ Simplenote/Classes/SPInteractiveTextStorage.h
@@ -1,11 +1,10 @@
 #import <UIKit/UIKit.h>
 
 
-extern NSString *const SPDefaultTokenName;
-extern NSString *const SPHeadlineTokenName;
-
 @interface SPInteractiveTextStorage : NSTextStorage
 
 @property (nonatomic, copy) NSDictionary<NSString *, NSDictionary<NSAttributedStringKey, id> *> *tokens;
+@property (nonatomic, copy) NSDictionary<NSAttributedStringKey, id> *defaultStyle;
+@property (nonatomic, copy) NSDictionary<NSAttributedStringKey, id> *headlineStyle;
 
 @end
