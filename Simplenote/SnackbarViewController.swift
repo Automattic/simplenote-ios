29e050b70eb8b3260f7f1ce0787c10cbe8ffcb25
Kevin L <kevin.lacoste@automattic.com>
2020-11-09 19:10:11 +0800

    Update XIB with StackView and subviews.
    Now includes the label and action button with basic layout constraints. Still needs sizing of the snackbar width if we want to shrink it to match the label + button title width.

--- Simplenote/SnackbarViewController.swift
+++ Simplenote/SnackbarViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class SnackbarViewController: UIViewController {
 	
+	@IBOutlet weak var messageLabel: UILabel!
+	@IBOutlet weak var actionButton: UIButton!
+	
 	deinit {
 		print("VC deinit.")
 	}
@@ -17,6 +20,10 @@ class SnackbarViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		view.layer.cornerRadius = 12
-		view.backgroundColor = .yellow
+		view.backgroundColor = .lightGray
+	}
+	
+	@IBAction func actionButtonTapped(_ sender: UIButton) {
+		print("Button tapped!")
 	}
 }
