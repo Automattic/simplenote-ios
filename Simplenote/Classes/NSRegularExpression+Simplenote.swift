3cbcdf9d4d373bd1175db3ae61688da62d728186
Evgeny Shurakov <eshurakov@users.noreply.github.com>
2020-11-11 10:52:40 +0100

    Remove excerpt regexp

--- Simplenote/Classes/NSRegularExpression+Simplenote.swift
+++ Simplenote/Classes/NSRegularExpression+Simplenote.swift
@@ -32,33 +32,3 @@ extension NSRegularExpression {
     @objc
     static let regexForChecklistsMarkerRangeIndex = 1
 }
-
-// MARK: - NSRegularExpression (Search excerpts)
-//
-extension NSRegularExpression {
-
-    /// Matches one of the keywords and a short text surrounding it
-    ///
-    /// - Parameters:
-    ///     - keywords: a non-empty list of keywords
-    ///
-    static func regexForExcerpt(withKeywords keywords: [String]) -> NSRegularExpression {
-        guard !keywords.isEmpty else {
-            return try! NSRegularExpression(pattern: "^.*{\(Constants.excerptLeadingLength + Constants.excerptTrailingLength)}", options: [])
-        }
-
-        let escapedKeywords = keywords.map(NSRegularExpression.escapedPattern).joined(separator: "|")
-        /// Patter explanation:
-        /// Leading: word boundary + word character + up to `excerptLeadingLength` word characters
-        /// Main: word boundary + optional word characters + one of the keywords + optional word characters + word boundary
-        /// Trailing: up to `excerptTrailingLength` word characters + word character + word boundary
-        let pattern = "(?:\\b\\w[\\w\\W]{0,\(Constants.excerptLeadingLength)})?\\b(?:\\w*(?:\(escapedKeywords))\\w*)\\b(?:[\\w\\W]{0,\(Constants.excerptTrailingLength)}\\w\\b)?"
-
-        return try! NSRegularExpression(pattern: pattern, options: [.caseInsensitive, .useUnicodeWordBoundaries])
-    }
-}
-
-private struct Constants {
-    static let excerptLeadingLength = 30
-    static let excerptTrailingLength = 300
-}
