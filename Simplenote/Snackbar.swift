98084dc5ba2a17af49e95520b58d8a20d073e9fc
Kevin L <kevin.lacoste@automattic.com>
2020-11-11 18:30:39 +0800

    Convert tabs to spaces.
    …for files modified/added in this branch.

--- Simplenote/Snackbar.swift
+++ Simplenote/Snackbar.swift
@@ -9,147 +9,147 @@
 import Foundation
 
 class Snackbar {
-	
-	private var message: String
-	
-	init(message: String = "Test message") {
-		self.message = message
-	}
-	
-	deinit {
-		print("Snackbar deinit")
-	}
-	
-	func show() {
-		print("Displaying snackbar")
-		SnackbarPresenter.shared.present(self)
-	}
+    
+    private var message: String
+    
+    init(message: String = "Test message") {
+        self.message = message
+    }
+    
+    deinit {
+        print("Snackbar deinit")
+    }
+    
+    func show() {
+        print("Displaying snackbar")
+        SnackbarPresenter.shared.present(self)
+    }
 }
 
-class SnackbarPresenter {
-	
-	static let shared = SnackbarPresenter()
-	
-	private var snackbar: Snackbar?
-	private var viewController: SnackbarViewController?
-	
-	// Prevent use outside the shared instance.
-	private init() {}
-	
-	deinit {
-		print("SnackbarPresenter deinit")
-		// Should never happen
-	}
-	
-	func present(_ sender: Snackbar) {
-		guard snackbar == nil else {
-			print("Already presenting. Please wait.")
-			return
-		}
-		
-		snackbar = sender
-		
-//		let snackView = prepareView()
-		let snackView = prepareViewFromXIB()
-		presentView(snackView)
-		
-		// Puts the view onscreen and returns.
-	}
-	
-	private func prepareView() -> UIView {
-		
-		// Prep a test view for display.
-		// Limit to 80% of the host window.
-		
-		let window = UIApplication.shared.keyWindow!
-		let width: CGFloat = window.frame.size.width * 0.8
-		let height: CGFloat = 60 // Should come from text size?
-		
-		let rect = CGRect(x: 0, y: 0, width: width, height: height)
-		let view = UIView(frame: rect)
-		view.backgroundColor = .lightGray
-		view.layer.cornerRadius = rect.height / 2
-		
-		return view
-	}
-	
-	private func prepareViewFromXIB() -> UIView {
-		
-		let vc = SnackbarViewController()
-		let view = vc.view!
-		
-		let frame = view.frame
-		let newFrame = CGRect(x: 0, y: 0, width: frame.width - 30, height: frame.height)
-		vc.view.frame = newFrame
-		
-		viewController = vc
-		
-		return view
-	}
+    class SnackbarPresenter {
 
-	private func presentView(_ view: UIView) {
-		
-		// Determine where the view goes in the host window.
-		
-		let window = UIApplication.shared.keyWindow!
-		
-		window.addSubview(view)
-		
-		// View center should be window width / 2 and window height - offset - (view height / 2)
-		
-		let viewHeight = view.frame.height
-		let offset: CGFloat = 50 // Magic number alert!
-		view.center.x = window.frame.size.width / 2
-		view.center.y = window.frame.size.height - offset - (viewHeight / 2.0) // Half the view height + the gap from view to screen edge.
-		print(view.center)
-		print(window.frame.height)
-		
-		// Now push the view down offscreen.
-		view.center.y = view.center.y + offset + viewHeight
-		print(view.center.y)
-		
-		UIView.animate(withDuration: 0.2, delay: 0.0, options: [], animations: {
-			view.center.y = view.center.y - offset - viewHeight
-		}) { _ in
-			print("Animation complete.")
-			
-			// Using the delay param results in the UIButton not receiving inputs.
-			// Using the asyncAfter allows the button to work correctly.
-			
-//			self.removeView(view)
-			self.removeViewAsync(view)
-		}
-	}
+    static let shared = SnackbarPresenter()
+
+    private var snackbar: Snackbar?
+    private var viewController: SnackbarViewController?
+
+    // Prevent use outside the shared instance.
+    private init() {}
+
+    deinit {
+        print("SnackbarPresenter deinit")
+        // Should never happen
+    }
+
+    func present(_ sender: Snackbar) {
+        guard snackbar == nil else {
+            print("Already presenting. Please wait.")
+            return
+        }
+        
+        snackbar = sender
+        
+    //		let snackView = prepareView()
+        let snackView = prepareViewFromXIB()
+        presentView(snackView)
+        
+        // Puts the view onscreen and returns.
+    }
+
+    private func prepareView() -> UIView {
+        
+        // Prep a test view for display.
+        // Limit to 80% of the host window.
+        
+        let window = UIApplication.shared.keyWindow!
+        let width: CGFloat = window.frame.size.width * 0.8
+        let height: CGFloat = 60 // Should come from text size?
+        
+        let rect = CGRect(x: 0, y: 0, width: width, height: height)
+        let view = UIView(frame: rect)
+        view.backgroundColor = .lightGray
+        view.layer.cornerRadius = rect.height / 2
+        
+        return view
+    }
+
+    private func prepareViewFromXIB() -> UIView {
+        
+        let vc = SnackbarViewController()
+        let view = vc.view!
+        
+        let frame = view.frame
+        let newFrame = CGRect(x: 0, y: 0, width: frame.width - 30, height: frame.height)
+        vc.view.frame = newFrame
+        
+        viewController = vc
+        
+        return view
+    }
+
+    private func presentView(_ view: UIView) {
+        
+        // Determine where the view goes in the host window.
+        
+        let window = UIApplication.shared.keyWindow!
+        
+        window.addSubview(view)
+        
+        // View center should be window width / 2 and window height - offset - (view height / 2)
+        
+        let viewHeight = view.frame.height
+        let offset: CGFloat = 50 // Magic number alert!
+        view.center.x = window.frame.size.width / 2
+        view.center.y = window.frame.size.height - offset - (viewHeight / 2.0) // Half the view height + the gap from view to screen edge.
+        print(view.center)
+        print(window.frame.height)
+        
+        // Now push the view down offscreen.
+        view.center.y = view.center.y + offset + viewHeight
+        print(view.center.y)
+        
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: [], animations: {
+            view.center.y = view.center.y - offset - viewHeight
+        }) { _ in
+            print("Animation complete.")
+            
+            // Using the delay param results in the UIButton not receiving inputs.
+            // Using the asyncAfter allows the button to work correctly.
+            
+    //			self.removeView(view)
+            self.removeViewAsync(view)
+        }
+    }
+
+    private func removeView(_ view: UIView) {
+        
+        // Results in action button not working.
+        UIView.animate(withDuration: 0.66, delay: 2.4, options: []) {
+            view.alpha = 0
+        } completion: { (Bool) in
+            view.removeFromSuperview()
+            print("Done fade.")
+
+            self.snackbar = nil
+            self.viewController = nil
+        }
+    }
 	
-	private func removeView(_ view: UIView) {
-		
-		// Results in action button not working.
-		UIView.animate(withDuration: 0.66, delay: 2.4, options: []) {
-			view.alpha = 0
-		} completion: { (Bool) in
-			view.removeFromSuperview()
-			print("Done fade.")
+    private func removeViewAsync(_ view: UIView) {
+        
+        // Allows action button to accept inputs.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.4) {
+            print("Hiding snackbar")
 
-			self.snackbar = nil
-			self.viewController = nil
-		}
-	}
-	
-	private func removeViewAsync(_ view: UIView) {
-		
-		// Allows action button to accept inputs.
-		DispatchQueue.main.asyncAfter(deadline: .now() + 2.4) {
-			print("Hiding snackbar")
+            UIView.animate(withDuration: 0.66, delay: 0.0, options: []) {
+                view.alpha = 0
+            } completion: { (Bool) in
+                view.removeFromSuperview()
+                print("Done fade.")
 
-			UIView.animate(withDuration: 0.66, delay: 0.0, options: []) {
-				view.alpha = 0
-			} completion: { (Bool) in
-				view.removeFromSuperview()
-				print("Done fade.")
-
-				self.snackbar = nil
-				self.viewController = nil
-			}
-		}
-	}
+                self.snackbar = nil
+                self.viewController = nil
+            }
+        }
+    }
 }
--- Simplenote/SnackbarViewController.swift
+++ Simplenote/SnackbarViewController.swift
@@ -9,21 +9,21 @@
 import UIKit
 
 class SnackbarViewController: UIViewController {
-	
-	@IBOutlet weak var messageLabel: UILabel!
-	@IBOutlet weak var actionButton: UIButton!
-	
-	deinit {
-		print("VC deinit.")
-	}
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		view.layer.cornerRadius = 12
-		view.backgroundColor = .lightGray
-	}
-	
-	@IBAction func actionButtonTapped(_ sender: UIButton) {
-		print("Button tapped!")
-	}
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var actionButton: UIButton!
+    
+    deinit {
+        print("VC deinit.")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.layer.cornerRadius = 12
+        view.backgroundColor = .lightGray
+    }
+    
+    @IBAction func actionButtonTapped(_ sender: UIButton) {
+        print("Button tapped!")
+    }
 }
