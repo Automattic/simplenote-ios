c0656935146257596cc139b35f1539dd5c035563
Evgeny Shurakov <eshurakov@users.noreply.github.com>
2021-01-20 12:39:38 +0100

    Extract AccountVerificationRemote and add tests for AccountVerificationController

---
+++ SimplenoteTests/Verification/AccountVerificationControllerTests.swift
@@ -0,0 +1,62 @@
+import XCTest
+@testable import Simplenote
+
+// MARK: - AccountVerificationControllerTests
+//
+class AccountVerificationControllerTests: XCTestCase {
+
+    private let email = UUID().uuidString
+    private lazy var remote = MockAccountVerificationRemote()
+    private lazy var controller = AccountVerificationController(email: email, remote: remote)
+
+    private lazy var invalidVerification: [String: Any] = [:]
+    private lazy var unverifiedVerification: [String: Any] = ["token": "321:0:123",
+                                                              "status": "verified"]
+    private lazy var inProgressVerification: [String: Any] = ["status": "sent"]
+    private lazy var verifiedVerification: [String: Any] = ["token": "\(email):0:123",
+                                                            "status": "verified"]
+
+    func testVerifyCallsRemoteWithProvidedEmail() {
+        // When
+        let expectedResult = Bool.random()
+        var verificationResult: Bool?
+        controller.verify { (result) in
+            verificationResult = result
+        }
+
+        remote.processVerification(for: email, with: expectedResult)
+
+        // Then
+        XCTAssertEqual(verificationResult, expectedResult)
+    }
+
+    func testInitialStateIsUnknown() {
+        XCTAssertEqual(controller.state, .unknown)
+    }
+
+    func testStateIsUnverifiedWhenUpdatingWithInvalidData() {
+        controller.update(with: nil)
+        XCTAssertEqual(controller.state, .unverified)
+
+        controller.update(with: "verified")
+        XCTAssertEqual(controller.state, .unverified)
+
+        controller.update(with: invalidVerification)
+        XCTAssertEqual(controller.state, .unverified)
+    }
+
+    func testStateIsUnverifiedIfTokenEmailDoesntMatchAccountEmail() {
+        controller.update(with: unverifiedVerification)
+        XCTAssertEqual(controller.state, .unverified)
+    }
+
+    func testStateIsInProgressIfStatusIsSent() {
+        controller.update(with: inProgressVerification)
+        XCTAssertEqual(controller.state, .verificationInProgress)
+    }
+
+    func testStateIsVerifiedIfTokenEmailMatchesAccountEmail() {
+        controller.update(with: verifiedVerification)
+        XCTAssertEqual(controller.state, .verified)
+    }
+}
