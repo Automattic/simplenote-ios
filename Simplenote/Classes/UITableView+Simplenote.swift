d2b389be28e0ae9dc6fca0ae7b32e51a4c54e623
Evgeny Shurakov <eshurakov@users.noreply.github.com>
2021-01-25 08:58:33 +0100

    Navigation helpers for table view

--- Simplenote/Classes/UITableView+Simplenote.swift
+++ Simplenote/Classes/UITableView+Simplenote.swift
@@ -46,3 +46,120 @@ extension UITableView {
         return dequeueReusableHeaderFooterView(withIdentifier: T.reuseIdentifier) as? T
     }
 }
+
+// MARK: - Navigation
+//
+extension UITableView {
+
+    /// Returns first index path from the first non-empty section
+    ///
+    var firstIndexPath: IndexPath? {
+        let indexPath = IndexPath(row: 0, section: 0)
+        if numberOfRows(inSection: 0) == 0 {
+            return nextIndexPath(after: indexPath)
+        }
+
+        return indexPath
+    }
+
+    /// Returns index path after currently selected index path
+    ///
+    var nextIndexPath: IndexPath? {
+        guard let indexPath = indexPathForSelectedRow else {
+            return nil
+        }
+
+        return nextIndexPath(after: indexPath)
+    }
+
+    /// Returns index path before currently selected index path
+    ///
+    var prevIndexPath: IndexPath? {
+        guard let indexPath = indexPathForSelectedRow else {
+            return nil
+        }
+
+        var row = indexPath.row
+        var section = indexPath.section
+
+        while true {
+            if row == 0 {
+                if section == 0 {
+                    return nil
+                }
+
+                section -= 1
+                row = numberOfRows(inSection: section)
+            } else {
+                return IndexPath(row: row - 1, section: section)
+            }
+        }
+    }
+
+    /// Selects row after currently selected row or first row if no row is currently selected
+    ///
+    func selectNextRow() {
+        guard let indexPath = (indexPathForSelectedRow != nil ? nextIndexPath : firstIndexPath) else {
+            return
+        }
+        deselectSelectedRow()
+        selectRow(at: indexPath, animated: false, scrollPosition: .none)
+        scrollRectToVisible(rectForRow(at: indexPath), animated: false)
+    }
+
+    /// Selects row before currently selected row or first row if no row is currently selected
+    ///
+    func selectPrevRow() {
+        guard let indexPath = prevIndexPath ?? firstIndexPath else {
+            return
+        }
+        deselectSelectedRow()
+        selectRow(at: indexPath, animated: false, scrollPosition: .none)
+        scrollRectToVisible(rectForRow(at: indexPath), animated: false)
+    }
+
+    /// Calls `didSelectRow` for a currently selected row
+    ///
+    func executeSelection() {
+        guard let indexPath = indexPathForSelectedRow else {
+            return
+        }
+
+        delegate?.tableView?(self, didSelectRowAt: indexPath)
+    }
+
+    /// Deselects selected row if any
+    ///
+    func deselectSelectedRow() {
+        guard let indexPath = indexPathForSelectedRow else {
+            return
+        }
+
+        deselectRow(at: indexPath, animated: false)
+    }
+}
+
+// MARK: - Navigation (Private)
+//
+private extension UITableView {
+    func nextIndexPath(after prevIndexPath: IndexPath) -> IndexPath? {
+        var row = prevIndexPath.row
+        var section = prevIndexPath.section
+
+        while true {
+            row += 1
+
+            if row >= numberOfRows(inSection: section) {
+                section += 1
+
+                if section >= numberOfSections {
+                    return nil
+                }
+
+                row = -1
+            } else {
+                return IndexPath(row: row, section: section)
+            }
+        }
+    }
+}
