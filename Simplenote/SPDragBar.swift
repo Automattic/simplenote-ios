9aebae84ee66a00d7e171d28f2dc14554828e091
Chaslie Scheer <charlie.scheer@automattic.com>
2021-01-26 13:55:23 +1300

    Created SPDragBar class and file

---
+++ Simplenote/SPDragBar.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class SPDragBar: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    func configure() {
+        self.backgroundColor = UIColor.black
+        self.alpha = 0.2
+        self.layer.cornerRadius = 2.5
+        self.layer.masksToBounds = true
+    }
+}
