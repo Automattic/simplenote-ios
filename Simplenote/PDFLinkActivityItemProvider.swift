276f1ccfc74ee0b818d1a4faf5a1b8e25696f294
Chaslie Scheer <charlie.scheer@automattic.com>
2021-02-03 11:46:41 +1300

    Created a class to provide ActivityViewController a link to a PDF file

--- Simplenote/PDFLinkActivityItemProvider.swift
+++ Simplenote/PDFLinkActivityItemProvider.swift
@@ -1,9 +1,24 @@
-//
-//  PDFLinkActivityItemProvider.swift
-//  Simplenote
-//
-//  Created by Charlie Scheer on 2/3/21.
-//  Copyright © 2021 Automattic. All rights reserved.
-//
-
 import Foundation
+
+public class PDFLinkActivityItemProvider : UIActivityItemProvider {
+    public let targetURL: URL
+    public let filename: String
+    public let note: Note
+    
+    public init(note: Note) {
+        self.note = note
+        self.filename = String(format: "%@.pdf", "Thisisastring")
+        self.targetURL = FileManager.documentsURL.appendingPathComponent(filename)
+        
+        super.init(placeholderItem: targetURL)
+    }
+    
+    public override var item: Any {
+        guard activityType?.rawValue != "com.apple.UIKit.activity.RemoteOpenInApplication",
+        let url = SimplenotePDFExporter().exportNoteToFiles(note: note, filename: filename) else {
+            return note.content as Any
+        }
+        
+        return url
+    }
+}
