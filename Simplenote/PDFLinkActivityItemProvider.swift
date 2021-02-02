c4584518bfbe5ad73b71e6fb31ef0be1ac4d79d2
Chaslie Scheer <charlie.scheer@automattic.com>
2021-02-03 11:44:41 +1300

    Added files for PDFExporter and PDFLinkProvider

---
+++ Simplenote/PDFLinkActivityItemProvider.swift
@@ -0,0 +1,9 @@
+//
+//  PDFLinkActivityItemProvider.swift
+//  Simplenote
+//
+//  Created by Charlie Scheer on 2/3/21.
+//  Copyright © 2021 Automattic. All rights reserved.
+//
+
+import Foundation
