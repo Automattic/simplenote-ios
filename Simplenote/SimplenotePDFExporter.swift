4d73c17e685dd256412ab50a3835a18c5a98997e
Chaslie Scheer <charlie.scheer@automattic.com>
2021-02-04 11:56:47 +1300

    refactored SimplenotePDFExprorter to only handle creating pdf from text

--- Simplenote/SimplenotePDFExporter.swift
+++ Simplenote/SimplenotePDFExporter.swift
@@ -1,39 +1,25 @@
 import Foundation
 
-class SimplenotePDFExporter {
-    func exportNoteToFiles(note: Note, filename: String) -> URL? {
-        let data = createPDFDataFromNote(note: note)
-        return writeNoteToDocuments(data: data, filename: filename)
-    }
-    
-    private func writeNoteToDocuments(data: Data, filename: String) -> URL? {
-        let targetURL = FileManager.documentsURL.appendingPathComponent(filename)
-        do {
-            try data.write(to: targetURL)
-        } catch {
-            NSLog("Note Exporter Failure: \(error)")
-            return nil
-        }
-        return targetURL
-    }
-    
-    private func createPDFDataFromNote(note: Note) -> Data {
+struct SimplenotePDFExporter {
+    public static func exportStringToPDFData(_ string: String) -> Data {
         let format = UIGraphicsPDFRendererFormat()
         
-        let pageWidth = 8.5 * 72.0
-        let pageHeight = 11 * 72.0
-        let pageRect = CGRect(x: 0, y: 0, width: pageWidth, height: pageHeight)
+        let pageRect = CGRect(x: 0, y: 0, width: Consts.pageWidth, height: Consts.pageHeight)
         let renderer = UIGraphicsPDFRenderer(bounds: pageRect, format: format)
         
         let data = renderer.pdfData { (context) in
-          context.beginPage()
-          let attributes = [
-            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16)
-          ]
-            if let text = note.content {
-                text.draw(at: CGPoint(x: 0, y: 0), withAttributes: attributes)
-            }
+            context.beginPage()
+            let attributes = [
+                NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16)
+            ]
+            string.draw(at: .zero, withAttributes: attributes)
         }
+        
         return data
     }
+}
+
+private struct Consts {
+    static let pageWidth = 8.5 * 72.0
+    static let pageHeight = 8.5 * 72.0
 }
