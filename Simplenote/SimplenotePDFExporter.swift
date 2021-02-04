22a863474ef94b4c8414d38f12f2a8a60fccf2d5
Chaslie Scheer <charlie.scheer@automattic.com>
2021-02-04 16:42:04 +1300

    Add multipage support to SimplenotePDFExporter

--- Simplenote/SimplenotePDFExporter.swift
+++ Simplenote/SimplenotePDFExporter.swift
@@ -1,27 +1,74 @@
 import Foundation
 
 struct SimplenotePDFExporter {
-    public static func exportStringToPDFData(_ string: String) -> Data {
-        let format = UIGraphicsPDFRendererFormat()
+    static func exportStringToPDFData(_ string: String) -> Data {
+        let currentText = NSAttributedString(string: string)
+        let pdfData = NSMutableData()
         
-        let renderer = UIGraphicsPDFRenderer(bounds: Constants.pageBounds, format: format)
+        UIGraphicsBeginPDFContextToData(pdfData, CGRect.zero, nil)
         
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.alignment = .natural
-        paragraphStyle.lineBreakMode = .byWordWrapping
+        let framesetter = CTFramesetterCreateWithAttributedString(currentText)
         
-        let data = renderer.pdfData { (context) in
-            context.beginPage()
-            let attributes = [
-                NSAttributedString.Key.paragraphStyle: paragraphStyle,
-                NSAttributedString.Key.font: UIFont.systemFont(ofSize: Constants.pdfFontSize)
-            ]
+        var currentRange = CFRangeMake(0, 0);
+        var currentPage = 0;
+        var done = false;
+        
+        repeat {
+            // Mark the beginning of a new page.
+            UIGraphicsBeginPDFPageWithInfo(CGRect(x: 0, y: 0, width: 612, height: 792), nil);
             
-            let attributedText = NSAttributedString(string: string, attributes: attributes)
-            attributedText.draw(in: Constants.textBounds)
-        }
+            // Draw a page number at the bottom of each page.
+            currentPage += 1;
+            
+            // Render the current page and update the current range to
+            // point to the beginning of the next page.
+            renderPagewithTextRange(currentRange: &currentRange, framesetter: framesetter)
+            
+            // If we're at the end of the text, exit the loop.
+            if (currentRange.location == CFAttributedStringGetLength(currentText)){
+                done = true;
+            }
+        } while (!done);
         
+        // Close the PDF context and write the contents out.
+        UIGraphicsEndPDFContext();
+        
+        let data = Data(pdfData)
         return data
+    }
+    
+    static func renderPagewithTextRange (currentRange: inout CFRange,  framesetter: CTFramesetter) {
+        // Get the graphics context.
+        if let currentContext = UIGraphicsGetCurrentContext(){
+            
+            // Put the text matrix into a known state. This ensures
+            // that no old scaling factors are left in place.
+            currentContext.textMatrix = CGAffineTransform.identity;
+            
+            // Create a path object to enclose the text. Use 72 point
+            // margins all around the text.
+            let frameRect = CGRect(x: 72, y: 72, width: 468, height: 648);
+            let framePath = CGMutablePath();
+            framePath.addRect(frameRect)
+            
+            // Get the frame that will do the rendering.
+            // The currentRange variable specifies only the starting point. The framesetter
+            // lays out as much text as will fit into the frame.
+            let frameRef = CTFramesetterCreateFrame(framesetter, currentRange, framePath, nil);
+            
+            // Core Text draws from the bottom-left corner up, so flip
+            // the current transform prior to drawing.
+            currentContext.translateBy(x: 0, y: 792);
+            currentContext.scaleBy(x: 1.0, y: -1.0);
+            
+            // Draw the frame.
+            CTFrameDraw(frameRef, currentContext);
+            
+            // Update the current range based on what was drawn.
+            currentRange = CTFrameGetVisibleStringRange(frameRef);
+            currentRange.location += currentRange.length;
+            currentRange.length = 0;
+        }
     }
 }
 
