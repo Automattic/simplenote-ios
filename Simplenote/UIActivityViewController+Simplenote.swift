151a48dde4f5705419dd4747fb052f88a167e4a6
Chaslie Scheer <charlie.scheer@automattic.com>
2021-02-09 13:30:08 +1300

    Updated ActivityVC to use refactored SPActivityItemSource

--- Simplenote/UIActivityViewController+Simplenote.swift
+++ Simplenote/UIActivityViewController+Simplenote.swift
@@ -13,9 +13,10 @@ extension UIActivityViewController {
         guard let content = note.content else {
             return nil
         }
+        let shareFilename = note.exportFilename()
 
         let print = SPSimpleTextPrintFormatter(text: content)
-        let source = SimplenoteActivityItemSource(note: note)
+        let source = SimplenoteActivityItemSource(content: content, filename: String(format: "%@.txt", shareFilename))
 
         self.init(activityItems: [print, source], applicationActivities: nil)
     }
