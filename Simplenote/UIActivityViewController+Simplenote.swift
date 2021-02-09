1f7d9acb4661665c3f788bc37a30157e10cdf2ed
Chaslie Scheer <charlie.scheer@automattic.com>
2021-02-10 07:56:37 +1300

    Added note for context about why we removed books from the share sheet

--- Simplenote/UIActivityViewController+Simplenote.swift
+++ Simplenote/UIActivityViewController+Simplenote.swift
@@ -19,6 +19,8 @@ extension UIActivityViewController {
 
         self.init(activityItems: [print, source], applicationActivities: nil)
         
+        // Share to ibooks feature that is added by the SimpleTextPrintFormatter requires a locally generated PDF or the share fails silently
+        // After much discussion the decision was to not implement a PDF generator into SN at this time, removing share to books as an option.
         excludedActivityTypes = [
             UIActivity.ActivityType.openInIBooks
         ]
