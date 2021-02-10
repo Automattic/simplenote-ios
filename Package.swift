401d3ff62f42dd97928eae4425156f0688291898
Wendy Chen <jaewchen@gmail.com>
2021-02-09 20:42:37 -0600

    Adding SwiftLint to Xcode via Swift Package Manager.

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "simplenote-tools-ios",
+    products: [],
+    dependencies: [
+	.package(url: "https://github.com/Realm/SwiftLint", from: "0.42.0")
+    ],
+    targets: []
+)
