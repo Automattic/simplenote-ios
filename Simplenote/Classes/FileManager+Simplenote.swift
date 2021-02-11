bafe51e55567880e30b8e244fff5f4ef38c11196
Jorge Leandro Perez <jorge.perez@automattic.com>
2021-02-11 12:01:47 -0300

    FileManager+Simplenote: Dropping static modifier

--- Simplenote/Classes/FileManager+Simplenote.swift
+++ Simplenote/Classes/FileManager+Simplenote.swift
@@ -6,7 +6,7 @@ extension FileManager {
 
      /// User's Document Directory
      ///
-     static var documentsURL: URL {
+     var documentsURL: URL {
          guard let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
              fatalError("Cannot Access User Documents Directory")
          }
