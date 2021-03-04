d2a19ce82b6f176356fc0006722de7ebad01e5bd
Charlie Scheer <charlie.scheer@automattic.com>
2021-03-04 14:51:59 +1300

    Added controller class from mockup to SN

--- Simplenote/NoticeController.swift
+++ Simplenote/NoticeController.swift
@@ -1,9 +1,66 @@
-//
-//  NoticeController.swift
-//  Simplenote
-//
-//  Created by Charlie Scheer on 3/4/21.
-//  Copyright © 2021 Automattic. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class NoticeController {
+    static let shared = NoticeController()
+
+    private var notices: [Notice] = []
+    private let noticePresenter = NoticePresenter()
+
+    private var activeViewIsBeingTouched: Bool = false
+
+    private init() { }
+
+    func present(_ notice: Notice) {
+        if noticePresenter.isPresenting {
+            notices.append(notice)
+            return
+        }
+
+        let noticeView = makeNoticeView(from: notice)
+
+        noticePresenter.presentNoticeView(noticeView) {
+            if !self.activeViewIsBeingTouched {
+                self.dismiss(noticeView)
+            }
+        }
+    }
+
+    private func dismiss(_ noticeView: NoticeView) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + Times.tapDelay) {
+            self.noticePresenter.dismissNotification {
+                if !self.notices.isEmpty {
+                    self.present(self.notices.removeFirst())
+                }
+            }
+        }
+    }
+
+    private func makeNoticeView(from notice: Notice) -> NoticeView {
+        let noticeView: NoticeView = NoticeView.instantiateFromNib()
+        noticeView.noticeLabel.text = notice.message
+        noticeView.noticeButton.setTitle(notice.action?.title, for: .normal)
+        noticeView.action = notice.action?.handler
+        noticeView.delegate = self
+
+        return noticeView
+    }
+}
+
+extension NoticeController: NoticePresentingDelegate {
+    func noticePressBegan() {
+        activeViewIsBeingTouched = true
+    }
+
+    func noticePressEnded() {
+        activeViewIsBeingTouched = false
+
+        guard let noticeView = noticePresenter.noticeView else {
+            return
+        }
+        dismiss(noticeView)
+    }
+}
+
+private struct Times {
+    static let tapDelay = TimeInterval(2)
+}
