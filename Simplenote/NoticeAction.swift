fb6acce6d5b13e541d18f2709e16a8c29c58a45c
Charlie Scheer <charlie.scheer@automattic.com>
2021-03-04 16:39:26 +1300

    Added enum and localized string options for notice actions

--- Simplenote/NoticeAction.swift
+++ Simplenote/NoticeAction.swift
@@ -1,6 +1,20 @@
 import Foundation
 
 struct NoticeAction {
-    let title: String
+    let title: ActionMessage
     let handler: () -> Void
+
+    func localizedTitle() -> String {
+        switch title {
+        case .retry:
+            return NSLocalizedString("Retry", comment: "Retry action title")
+        case .undo:
+            return NSLocalizedString("Undo", comment: "Undo action title")
+        }
+    }
+}
+
+enum ActionMessage {
+    case undo
+    case retry
 }
