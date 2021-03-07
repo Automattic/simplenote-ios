151e0feda2b4e382a103d9bb77a35a00a225e4d3
Charlie Scheer <charlie.scheer@automattic.com>
2021-03-07 14:43:31 +1300

    Updated calculation for untouchable view to pass taps

--- Simplenote/UntouchableView.swift
+++ Simplenote/UntouchableView.swift
@@ -3,7 +3,7 @@ import UIKit
 class UntouchableView: UIView {
     override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
         let visibleViews = subviews.filter { view -> Bool in
-            return view.alpha >= 0.01 && !view.isHidden && view.isUserInteractionEnabled
+            return view.alpha > .zero && !view.isHidden && view.isUserInteractionEnabled
         }
         for view in visibleViews {
             let relativePoint = convert(point, to: view)
