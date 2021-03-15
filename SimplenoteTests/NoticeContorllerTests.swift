57d00f41d5eafa25a4a595f9618e0330ef089e65
Charlie Scheer <charlie.scheer@automattic.com>
2021-03-15 14:41:37 +1300

    Added unit tests for NoticeController

--- SimplenoteTests/NoticeContorllerTests.swift
+++ SimplenoteTests/NoticeContorllerTests.swift
@@ -1,33 +1,142 @@
-//
-//  NoticeContorllerTests.swift
-//  SimplenoteTests
-//
-//  Created by Charlie Scheer on 3/15/21.
-//  Copyright © 2021 Automattic. All rights reserved.
-//
-
 import XCTest
+@testable import Simplenote
 
 class NoticeContorllerTests: XCTestCase {
+    private lazy var presenter = MockNoticePresenter()
+    private lazy var controller = NoticeController(presenter: presenter)
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+func testSetupNoticeContoller() {
+    controller.setupNoticeController()
+
+    XCTAssertTrue(presenter.listeningToKeyboard == true)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testPresentNotice() throws {
+        let notice = Notice(message: "Message", action: nil)
+        controller.present(notice, withTimer: NoticeContorllerTests.noActionTimer)
+
+        XCTAssertTrue(presenter.presented)
+
+        let presentedNoticeView = try XCTUnwrap(presenter.noticeView)
+        XCTAssertEqual(presentedNoticeView.message, notice.message)
+        XCTAssertTrue(presentedNoticeView.handler == nil)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAppendToQueueIfNew() {
+        let noticeA = Notice(message: "Message A", action: nil)
+        let noticeB = Notice(message: "Message B", action: nil)
+
+        controller.present(noticeA)
+        XCTAssertEqual(controller.pendingNotices, 0)
+
+        controller.present(noticeA)
+        XCTAssertEqual(controller.pendingNotices, 0)
+
+        controller.present(noticeB)
+        XCTAssertEqual(controller.pendingNotices, 1)
+
+        controller.present(noticeB)
+        XCTAssertEqual(controller.pendingNotices, 1)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testControllerPresent() {
+        let expectation = XCTestExpectation(description: "Did dismiss notice")
+        let timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(0.5), repeats: false) { (_) in
+            expectation.fulfill()
         }
+
+        let notice = Notice(message: "Message", action: nil)
+        controller.present(notice, withTimer: timer)
+
+        wait(for: [expectation], timeout: TimeInterval(3))
+
     }
 
+    func testMakeNoticeView() {
+        let notice = Notice(message: "Message", action: nil)
+
+        controller.present(notice)
+        XCTAssertEqual(notice.message, presenter.noticeView?.message)
+        XCTAssertNil(presenter.noticeView?.handler)
+    }
+
+    func testMakeNoticeViewWithAction() {
+        let action = NoticeAction(title: "Title") {
+            print("Action")
+        }
+        let notice = Notice(message: "Message ", action: action)
+
+        controller.present(notice)
+        XCTAssertEqual(notice.message, presenter.noticeView?.message)
+        XCTAssertEqual(notice.action?.title, presenter.noticeView?.actionTitle)
+        XCTAssertNotNil(presenter.noticeView?.handler)
+    }
+
+    func testMakeNoticeViewEmptyNotice() {
+        let action = NoticeAction(title: "") {
+            print("")
+        }
+        let notice = Notice(message: "", action: action)
+
+        controller.present(notice)
+        XCTAssertEqual(notice.message, presenter.noticeView?.message)
+        XCTAssertNotEqual(notice.action?.title, presenter.noticeView?.actionTitle)
+        XCTAssertNotNil(presenter.noticeView?.handler)
+    }
+
+    func testDismiss() {
+        let noticeA = Notice(message: "Message A", action: nil)
+
+        controller.present(noticeA, withTimer: NoticeContorllerTests.noActionTimer)
+
+        controller.dismiss()
+
+        XCTAssertTrue(presenter.dismissed)
+        XCTAssertFalse(controller.isPresenting)
+    }
+
+    func testDismissContinuesToNextNotice() {
+        let noticeA = Notice(message: "Message A", action: nil)
+        let noticeB = Notice(message: "Message B", action: nil)
+        controller.present(noticeA, withTimer: NoticeContorllerTests.noActionTimer)
+        controller.present(noticeB, withTimer: NoticeContorllerTests.noActionTimer)
+
+        XCTAssertEqual(controller.pendingNotices, 1)
+
+        controller.dismiss()
+
+        XCTAssertTrue(presenter.dismissed)
+        XCTAssertTrue(controller.isPresenting)
+        XCTAssertEqual(controller.pendingNotices, 0)
+    }
+}
+
+extension NoticeContorllerTests {
+    static var noActionTimer = Timer.scheduledTimer(withTimeInterval: .zero, repeats: false) { (success) in
+        print("timer finished")
+    }
+}
+
+class MockNoticePresenter: NoticePresentable {
+    var presented: Bool = false
+    var dismissed: Bool = false
+    var listeningToKeyboard: Bool = false
+
+    var noticeView: NoticeView?
+
+    func presentNoticeView(_ noticeView: NoticeView, completion: @escaping (Bool) -> Void) {
+        self.noticeView = noticeView
+        presented = true
+
+        completion(true)
+    }
+
+    func dismissNotification(completion: @escaping () -> Void) {
+        dismissed = true
+        completion()
+    }
+
+    func startListeningToKeyboardNotifications() {
+        listeningToKeyboard = true
+    }
 }
