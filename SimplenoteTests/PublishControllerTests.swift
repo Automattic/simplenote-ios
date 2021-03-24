34932f4f454ab55b08eb89bc32ae84a691772ee2
Charlie Scheer <charlie.scheer@automattic.com>
2021-03-24 16:36:13 +1300

    Added tests to PublishControllerTests for responding to changes

--- SimplenoteTests/PublishControllerTests.swift
+++ SimplenoteTests/PublishControllerTests.swift
@@ -3,15 +3,14 @@ import XCTest
 
 private let storage = MockupStorageManager()
 private let publishController = PublishController()
-private var note: Note = storage.insertSampleNote()
+private let mockAppDelegate = MockAppDelegate(publishController: publishController)
+private let callbackMap = [String: PublishListenWrapper]()
 
 class PublishControllerTests: XCTestCase {
 
-    override func setUp() {
-        note.simperiumKey = "ABCDEF123456"
-    }
-
     func testUpdatePublishExitsIfNoChangeInState() {
+        let note = storage.insertSampleNote(simperiumKey: TestConstants.simperiumKey)
+
         publishController.updatePublishState(for: note, to: false) { (_) in
             XCTFail("Callback should not be called")
         }
@@ -20,6 +19,7 @@ class PublishControllerTests: XCTestCase {
     }
 
     func testUpdatePublishStateToPublishing() {
+        let note = storage.insertSampleNote(simperiumKey: TestConstants.simperiumKey)
         publishController.updatePublishState(for: note, to: true) { (state) in
             XCTAssertEqual(state, .publishing)
         }
@@ -28,11 +28,53 @@ class PublishControllerTests: XCTestCase {
     }
 
     func testUpdatePublishStateToUnpublishing() {
+        let note = storage.insertSampleNote(simperiumKey: TestConstants.simperiumKey)
         publishController.updatePublishState(for: note, to: false) { (state) in
             XCTAssertEqual(state, .unpublishing)
         }
         XCTAssertFalse(note.published)
         XCTAssertEqual(note.publishURL, "")
     }
+
+    func testPublishStateIsChangedToPublishedWhenCalledFromListener() {
+        let note = storage.insertSampleNote(simperiumKey: TestConstants.simperiumKey)
+        var publishState: PublishState = .unpublished
+        publishController.updatePublishState(for: note, to: true) { (state) in
+            publishState = state
+        }
+
+        mockAppDelegate.update(note: note, to: true, with: TestConstants.simperiumKey, with: "ABC123")
+
+        XCTAssertEqual(publishState, .published)
+    }
+
+    func testPublishStateIsChangedToUnpublishedWhenCalledFromListener() {
+        let note = storage.insertSampleNote(simperiumKey: TestConstants.simperiumKey)
+        var publishState: PublishState = .unpublished
+        publishController.updatePublishState(for: note, to: true) { (state) in
+            publishState = state
+        }
+
+        mockAppDelegate.update(note: note, to: false, with: TestConstants.simperiumKey, with: "ABC123")
+
+        XCTAssertEqual(publishState, .unpublished)
+    }
 }
 
+private struct TestConstants {
+    static let simperiumKey = "ABCDEF123456"
+}
+
+class MockAppDelegate {
+    let publishController: PublishController
+
+    init(publishController: PublishController) {
+        self.publishController = publishController
+    }
+
+    func update(note: Note, to published: Bool, with key: String, with url: String) {
+        note.publishURL = "ABC123"
+        note.published = published
+        publishController.didReceiveUpdateFromSimperium(for: key as NSString)
+    }
+}
