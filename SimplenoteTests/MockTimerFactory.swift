c1cc55f73c909eafed57802f1fc38f5ef6926ef2
Charlie Scheer <charlie.scheer@automattic.com>
2021-03-24 16:33:11 +1300

    Moved MockTimerFactory to it's own file

--- SimplenoteTests/MockTimerFactory.swift
+++ SimplenoteTests/MockTimerFactory.swift
@@ -1,9 +1,29 @@
-//
-//  MockTimerFactory.swift
-//  SimplenoteTests
-//
-//  Created by Charlie Scheer on 3/24/21.
-//  Copyright © 2021 Automattic. All rights reserved.
-//
+@testable import Simplenote
 
-import Foundation
+class MockTimerFactory: TimerFactory {
+    var timer: MockTimer?
+
+    override func scheduledTimer(with timeInterval: TimeInterval, completion: @escaping () -> Void) -> Timer {
+        let timer = MockTimer()
+        self.timer = timer
+        timer.completion = completion
+        return timer
+    }
+}
+
+class MockTimer: Timer {
+    var completion: (() -> Void)?
+
+    convenience init(completion: (() -> Void)? = nil) {
+        self.init()
+        self.completion = completion
+    }
+
+    override func fire() {
+        completion?()
+    }
+
+    override func invalidate() {
+        completion = nil
+    }
+}
