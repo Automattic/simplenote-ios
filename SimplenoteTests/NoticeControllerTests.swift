5354af26bba2ba6310cbde83b79df773f65b0346
Charlie Scheer <charlie.scheer@automattic.com>
2021-03-24 16:34:33 +1300

    Removed MockTimerFactory from NoticeControllerTests

--- SimplenoteTests/NoticeControllerTests.swift
+++ SimplenoteTests/NoticeControllerTests.swift
@@ -216,31 +216,3 @@ class MockNoticePresenter: NoticePresenter {
         listeningToKeyboard = true
     }
 }
-
-class MockTimerFactory: TimerFactory {
-    var timer: MockTimer?
-
-    override func scheduledTimer(with timeInterval: TimeInterval, completion: @escaping () -> Void) -> Timer {
-        let timer = MockTimer()
-        self.timer = timer
-        timer.completion = completion
-        return timer
-    }
-}
-
-class MockTimer: Timer {
-    var completion: (() -> Void)?
-
-    convenience init(completion: (() -> Void)? = nil) {
-        self.init()
-        self.completion = completion
-    }
-
-    override func fire() {
-        completion?()
-    }
-
-    override func invalidate() {
-        completion = nil
-    }
-}
