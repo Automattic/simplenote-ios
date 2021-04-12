00f4fcbaf53e6bb6e314f73bc5e181443f3ea792
Charlie Scheer <charlie.scheer@automattic.com>
2021-04-13 09:44:18 +1200

    Removed dateSortPrefix from note list

--- Simplenote/Classes/SPNoteListViewController+Extensions.swift
+++ Simplenote/Classes/SPNoteListViewController+Extensions.swift
@@ -595,18 +595,7 @@ private extension SPNoteListViewController {
                 return nil
         }
 
-        return dateSortPrefix() + DateFormatter.listDateFormatter.string(from: date)
-    }
-
-    private func dateSortPrefix() -> String {
-        switch notesListController.sortMode {
-        case .createdNewest, .createdOldest:
-            return "Created: "
-        case .modifiedNewest, .modifiedOldest:
-            return "Modified: "
-        default:
-            return ""
-        }
+        return DateFormatter.listDateFormatter.string(from: date)
     }
 }
 
