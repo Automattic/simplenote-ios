532787d2ce487d23a6264a57d0eca73f3f59f86d
Gio Lodi <giovanni.lodi42@gmail.com>
2021-04-13 15:06:04 +1000

    Create new framework to host UITestHelpers
    I decided not to add a test target for the moment.
    
    When we'll extract this into a dedicated project, we shall add at least
    a dummy UI app to test it.
    
    For the moment, since it will only contains code consumed by the test
    targets in this project, we can skip the tests.

---
+++ UITestHelpers/UITestHelpers.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for UITestHelpers.
+FOUNDATION_EXPORT double UITestHelpersVersionNumber;
+
+//! Project version string for UITestHelpers.
+FOUNDATION_EXPORT const unsigned char UITestHelpersVersionString[];
