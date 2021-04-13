ea02ab718b429772918903bb2048c09d633308f5
Gio Lodi <giovanni.lodi42@gmail.com>
2021-04-13 16:24:52 +1000

    Update isDeviceIPhone8Plus access level to public in order to compile
    How did it work before?!

--- UITestHelpers/XCUIApplication+Device.swift
+++ UITestHelpers/XCUIApplication+Device.swift
@@ -2,9 +2,7 @@ import XCTest
 
 extension XCUIApplication {
 
-    // Note: I would have expected either this method or the whole extension to have public
-    // access level, but somehow the screenshots target builds anyways.
-    func isDeviceIPhone8Plus(_ device: XCUIDevice = .shared) -> Bool {
+    public func isDeviceIPhone8Plus(_ device: XCUIDevice = .shared) -> Bool {
         let iPhone8PlusScreenHeight = CGFloat(736)
 
         let frame = windows.element(boundBy: 0).frame
