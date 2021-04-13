b87a1b724a2e348cbe0eaede3ffea02adbf0417a
Gio Lodi <giovanni.lodi42@gmail.com>
2021-04-13 15:15:54 +1000

    Use UITestHelpers XCUIApplication extension in screenshots tests
    This is a proof of concept of how extracting and sharing general purpose
    UI test helpers would look like.

--- SimplenoteScreenshots/SimplenoteScreenshots.swift
+++ SimplenoteScreenshots/SimplenoteScreenshots.swift
@@ -1,3 +1,4 @@
+import UITestHelpers
 import XCTest
 
 class SimplenoteScreenshots: XCTestCase {
@@ -300,20 +301,6 @@ extension XCUIElement {
 
         if let string = previousPasteboardContents {
             UIPasteboard.general.string = string
-        }
-    }
-}
-
-extension XCUIApplication {
-
-    func isDeviceIPhone8Plus(_ device: XCUIDevice = .shared) -> Bool {
-        let iPhone8PlusScreenHeight = CGFloat(736)
-
-        let frame = windows.element(boundBy: 0).frame
-
-        switch device.orientation {
-        case .landscapeLeft, .landscapeRight: return frame.width == iPhone8PlusScreenHeight
-        case _: return frame.height == iPhone8PlusScreenHeight
         }
     }
 }
--- UITestHelpers/XCUIApplication+Device.swift
+++ UITestHelpers/XCUIApplication+Device.swift
@@ -2,6 +2,8 @@ import XCTest
 
 extension XCUIApplication {
 
+    // Note: I would have expected either this method or the whole extension to have public
+    // access level, but somehow the screenshots target builds anyways.
     func isDeviceIPhone8Plus(_ device: XCUIDevice = .shared) -> Bool {
         let iPhone8PlusScreenHeight = CGFloat(736)
 
