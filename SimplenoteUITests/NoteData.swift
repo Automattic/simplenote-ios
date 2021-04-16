57a1f24867f5cec3dcdcfddcefb9c6a3e7f2a268
Gio Lodi <giovanni.lodi42@gmail.com>
2021-04-16 16:35:37 +1000

    Use private(set) for var tags property in NoteData
    This way, we can use the compiler-generated initializer while also
    keeping the instance immutable for consumers, which is appropriate for a
    DTO-like type such as this one.
    
    See https://github.com/Automattic/simplenote-ios/pull/1240#discussion_r612848495

--- SimplenoteUITests/NoteData.swift
+++ SimplenoteUITests/NoteData.swift
@@ -2,7 +2,7 @@
 struct NoteData {
     let name: String
     let content: String
-    var tags: [String] = []
+    private(set) var tags: [String] = []
 
     var formattedForAutomatedInput: String { "\(name)\n\n\(content)" }
 }
