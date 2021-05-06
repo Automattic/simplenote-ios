8e37eefd3a2bc0b57ec147140d303ee18754c7ca
Charlie Scheer <charlie.scheer@automattic.com>
2021-05-07 09:40:29 +1200

    updated notice view to size correctly

--- Simplenote/NoticeView.swift
+++ Simplenote/NoticeView.swift
@@ -111,3 +111,34 @@ private struct Constants {
     static let cornerRadius = CGFloat(25)
     static let nibName = "NoticeView"
 }
+
+
+class MultilineTitleButton: UIButton {
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    func commonInit() -> Void {
+        self.titleLabel?.numberOfLines = 0
+        self.titleLabel?.textAlignment = .center
+        self.setContentHuggingPriority(UILayoutPriority.defaultLow + 1, for: .vertical)
+        self.setContentHuggingPriority(UILayoutPriority.defaultLow + 1, for: .horizontal)
+    }
+
+    override var intrinsicContentSize: CGSize {
+        let size = self.titleLabel!.intrinsicContentSize
+        return CGSize(width: size.width + contentEdgeInsets.left + contentEdgeInsets.right, height: size.height + contentEdgeInsets.top + contentEdgeInsets.bottom)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        titleLabel?.preferredMaxLayoutWidth = self.titleLabel!.frame.size.width
+    }
+}
