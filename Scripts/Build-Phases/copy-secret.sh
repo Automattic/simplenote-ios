20fb58a03df11fcbdfd5536424e82ec223712d18
Gio Lodi <giovanni.lodi42@gmail.com>
2021-05-20 04:52:07 +1000

    Centralize step to copy secrets

--- Scripts/Build-Phases/copy-secret.sh
+++ Scripts/Build-Phases/copy-secret.sh
@@ -17,11 +17,18 @@ if [ $GIT_IGNORE_EXIT_CODE -ne 0 ]; then
     exit 1
 fi
 
+# From now on, fail the script if there's an error.
+# We didn't do it above because we wanted to track the `git check-ignore` exit
+# code.
+set -e
+
 if [ ! -f ${SOURCE_PATH} ]; then
     echo "error: Unable to copy credentials. Could not find ${SOURCE_PATH}."
     exit 1
 else
     echo "Copying credentials..."
-    mkdir -p ${TARGET_FOLDER}
+    mkdir -p $(dirname $TARGET_PATH)
     cp ${SOURCE_PATH} ${TARGET_PATH}
 fi
+
+echo "✅ Copied secret at ${SOURCE_PATH} to ${TARGET_PATH}"
