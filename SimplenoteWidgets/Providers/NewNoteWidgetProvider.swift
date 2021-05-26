a31ed9f97ac70fc46c9ebc6b8a126a01652154b4
Charlie Scheer <charlie.scheer@automattic.com>
2021-05-27 09:53:56 +1200

    Refactored different parts of the new note widget into separate files

--- SimplenoteWidgets/Providers/NewNoteWidgetProvider.swift
+++ SimplenoteWidgets/Providers/NewNoteWidgetProvider.swift
@@ -10,25 +10,22 @@ import WidgetKit
 
 struct NewNoteWidgetEntry: TimelineEntry {
     var date: Date
-    var title: String
 }
 
 struct NewNoteTimelineProvider: TimelineProvider {
+    typealias Entry = NewNoteWidgetEntry
+
     func placeholder(in context: Context) -> NewNoteWidgetEntry {
-        return NewNoteWidgetEntry(date: Date(), title: "Title")
+        return NewNoteWidgetEntry(date: Date())
     }
 
     func getSnapshot(in context: Context, completion: @escaping (NewNoteWidgetEntry) -> Void) {
-        let entry = NewNoteWidgetEntry(date: Date(), title: "Title")
+        let entry = NewNoteWidgetEntry(date: Date())
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<NewNoteWidgetEntry>) -> Void) {
-        let timeline = Timeline(entries: [NewNoteWidgetEntry(date: Date(), title: "Title")], policy: .never)
+        let timeline = Timeline(entries: [NewNoteWidgetEntry(date: Date())], policy: .never)
         completion(timeline)
     }
-
-    typealias Entry = NewNoteWidgetEntry
-
-
 }
---
+++ SimplenoteWidgets/Widgets/NewNoteWidget.swift
@@ -0,0 +1,29 @@
+//
+//  SwiftUIView.swift
+//  Simplenote
+//
+//  Created by Charlie Scheer on 5/26/21.
+//  Copyright © 2021 Automattic. All rights reserved.
+//
+
+import SwiftUI
+import WidgetKit
+
+@available(iOS 14.0, *)
+struct NewNoteWidget: Widget {
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: Constants.configurationKind, provider: NewNoteTimelineProvider()) { entry in
+            NewNoteWidgetView(entry: entry)
+        }
+        .configurationDisplayName(Constants.displayName)
+        .description(Constants.description)
+        .supportedFamilies([.systemSmall])
+
+    }
+}
+
+private struct Constants {
+    static let configurationKind = "NewNoteWidget"
+    static let displayName = "New Note"
+    static let description = "Create a new note instantly with one tap."
+}
