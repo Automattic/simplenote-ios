d685ef249be738e10a75e0db3141bf2965293b56
Jorge Leandro Perez <jorge.perez@automattic.com>
2021-05-27 13:28:58 -0300

    Updates Bundle's Public API

--- Simplenote/Classes/Bundle+Simplenote.swift
+++ Simplenote/Classes/Bundle+Simplenote.swift
@@ -2,15 +2,43 @@ import Foundation
 import UIKit
 
 
-/// Bundle: Simplenote Methods
-///
+// MARK: - Bundle: Simplenote Methods
+//
 extension Bundle {
 
     /// Returns the Bundle Short Version String.
     ///
     @objc
     var shortVersionString: String {
-        let version = infoDictionary?["CFBundleShortVersionString"] as? String
+        let version = infoDictionary?[Keys.shortVersionString] as? String
         return version ?? ""
     }
+
+    /// AppEngine Base URL
+    ///
+    var engineBaseURL: String? {
+        infoDictionary?[Keys.engineBaseURL] as? String
+    }
+
+    /// Simperium: Authentication Endpoint
+    ///
+    var simperiumAuthURL: String? {
+        infoDictionary?[Keys.simperiumAuthURL] as? String
+    }
+
+    /// Simperium: Host Header
+    ///
+    var simperiumHost: String? {
+        infoDictionary?[Keys.simperiumHost] as? String
+    }
+}
+
+
+// MARK: - Private Keys
+//
+private enum Keys {
+    static let engineBaseURL        = "APP_ENGINE_BASE_URL"
+    static let simperiumAuthURL     = "AUTH_URL"
+    static let simperiumHost        = "AUTH_HOST"
+    static let shortVersionString   = "CFBundleShortVersionString"
 }
