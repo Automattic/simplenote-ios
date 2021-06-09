03ae24141018863447297a795d724ebfecda284f
Charlie Scheer <charlie.scheer@automattic.com>
2021-06-09 12:09:47 +1200

    Added basic setup for an intents extension for dynamic widget selection

---
+++ SimplenoteIntents/IntentHandler.swift
@@ -0,0 +1,26 @@
+import Intents
+
+class IntentHandler: INExtension {
+    override func handler(for intent: INIntent) -> Any {
+        return self
+    }
+}
+
+extension IntentHandler: SPNoteWidgetIntentHandling {
+    func provideNoteOptionsCollection(for intent: SPNoteWidgetIntent, with completion: @escaping (INObjectCollection<SPNote>?, Error?) -> Void) {
+        // This is placeholder code to confirm the dynamic intent selection is working
+        // TODO: add logic to fetch the available notes for account
+
+        let placeholderNoteData = ["Note 1", "Note 2", "Note 3", "Note 4"]
+
+        var notes: [SPNote] = []
+
+        for placeholder in placeholderNoteData {
+            let spNote = SPNote(identifier: placeholder, display: placeholder)
+            notes.append(spNote)
+        }
+
+        let collection = INObjectCollection(items: notes)
+        completion(collection, nil)
+    }
+}
