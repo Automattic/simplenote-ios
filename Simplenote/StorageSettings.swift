98d6082a83106284f884d136fcce124a67c8da24
Charlie Scheer <charlie.scheer@automattic.com>
2021-07-05 12:47:23 +1200

    Changed FileManager to be a stored property in StorageSettings

--- Simplenote/StorageSettings.swift
+++ Simplenote/StorageSettings.swift
@@ -3,24 +3,34 @@ import Foundation
 @objcMembers
 class StorageSettings: NSObject {
 
+    private let fileManager: FileManager
+
+    init(fileManager: FileManager = FileManager.default) {
+        self.fileManager = fileManager
+    }
+
     /// URL for the managed object model resource
     ///
     let modelURL = Bundle.main.url(forResource: Constants.resourceName, withExtension: Constants.resourceType)!
 
     /// In app core data storage URL
     ///
-    let legacyStorageURL = FileManager.default.documentsURL.appendingPathComponent(Constants.sqlFile)
+    var legacyStorageURL: URL {
+        fileManager.documentsURL.appendingPathComponent(Constants.sqlFile)
+    }
 
     var legacyStorageExists: Bool {
-        FileManager.default.fileExists(atPath: legacyStorageURL.path)
+        fileManager.fileExists(atPath: legacyStorageURL.path)
     }
 
     /// URL for core data storage in shared app group documents directory
     ///
-    let sharedStorageURL = FileManager.default.sharedContainerURL.appendingPathComponent(Constants.sqlFile)
+    var sharedStorageURL: URL {
+        fileManager.sharedContainerURL.appendingPathComponent(Constants.sqlFile)
+    }
 
     var sharedStorageExists: Bool {
-        FileManager.default.fileExists(atPath: sharedStorageURL.path)
+        fileManager.fileExists(atPath: sharedStorageURL.path)
     }
 
     var storageURL: URL {
