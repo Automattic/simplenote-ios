06ba60f27b0c95426cc6de77dd2648b351a661d7
Charlie Scheer <charlie.scheer@automattic.com>
2021-07-11 11:54:50 +1200

    Refactored getting the root bundle identifier for extensions

--- Simplenote/Classes/FileManager+Simplenote.swift
+++ Simplenote/Classes/FileManager+Simplenote.swift
@@ -23,19 +23,6 @@ extension FileManager {
 
 private struct Constants {
     static let defaultBundleIdentifier = "com.codality.NotationalFlow"
-    static let groupIdentifier = rootBundleIdentifier() ?? Constants.defaultBundleIdentifier
+    static let groupIdentifier = Bundle.main.rootBundleIdentifier ?? Constants.defaultBundleIdentifier
     static let sharedDirectoryDomain = "group."
-
-    static func rootBundleIdentifier() -> String? {
-        if Bundle.main.bundleURL.pathExtension != "appex" {
-            return Bundle.main.bundleIdentifier
-        }
-
-        let url = Bundle.main.bundleURL.deletingLastPathComponent().deletingLastPathComponent()
-
-        guard let bundle = Bundle(url: url) else {
-            return nil
-        }
-        return bundle.object(forInfoDictionaryKey: "CFBundleIdentifier") as? String
-    }
 }
