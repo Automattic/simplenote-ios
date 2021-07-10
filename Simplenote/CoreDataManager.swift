7997bba956552ea21198e3124b8f3ab1ad2a7fcd
Charlie Scheer <charlie.scheer@automattic.com>
2021-07-11 11:38:40 +1200

    Refactored core data manager initializing method into init

--- Simplenote/CoreDataManager.swift
+++ Simplenote/CoreDataManager.swift
@@ -4,19 +4,15 @@ import CoreData
 @objcMembers
 class CoreDataManager: NSObject {
 
-    init(_ storageURL: URL, storageSettings: StorageSettings = StorageSettings()) throws {
-        super.init()
-
-        try setupCoreDataStack(at: storageURL, modelURL: storageSettings.modelURL)
-    }
-
     // MARK: Core Data
     private(set) var managedObjectModel: NSManagedObjectModel!
     private(set) var managedObjectContext: NSManagedObjectContext!
     private(set) var persistentStoreCoordinator: NSPersistentStoreCoordinator!
 
-    private func setupCoreDataStack(at url: URL, modelURL: URL) throws {
-        guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
+    init(_ storageURL: URL, storageSettings: StorageSettings = StorageSettings()) throws {
+        super.init()
+
+        guard let mom = NSManagedObjectModel(contentsOf: storageSettings.modelURL) else {
             fatalError("Cannot load model")
         }
         managedObjectModel = mom
@@ -28,12 +24,10 @@ class CoreDataManager: NSObject {
         let options = [NSMigratePersistentStoresAutomaticallyOption: true,
                        NSInferMappingModelAutomaticallyOption: true]
 
-        // Testing logs
-        //
-        NSLog("🎯 Loading PersistentStore at URL: \(url)")
+        NSLog("🎯 Loading PersistentStore at URL: \(storageURL)")
         try psc.addPersistentStore(ofType: NSSQLiteStoreType,
                                        configurationName: nil,
-                                       at: url,
+                                       at: storageURL,
                                        options: options)
 
         persistentStoreCoordinator = psc
