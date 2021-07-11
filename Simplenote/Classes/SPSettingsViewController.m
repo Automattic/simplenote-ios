7b64248e28c0f62e7c693d491b6f40e5584d1179
Charlie Scheer <charlie.scheer@automattic.com>
2021-07-12 10:24:08 +1200

    Changed delete account button to call alert methods

--- Simplenote/Classes/SPSettingsViewController.m
+++ Simplenote/Classes/SPSettingsViewController.m
@@ -556,7 +556,7 @@ typedef NS_ENUM(NSInteger, SPOptionsDebugRow) {
         } case SPOptionsViewSectionsDelete: {
             switch (indexPath.row) {
                 case SPOptionsDeleteRowTitle: {
-                    NSLog(@"Delete Pressed");
+                    [self presentDeleteAccountConfirmation];
                     break;
                 }
             }
