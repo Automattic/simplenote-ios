202217973afb120529f89b3ff73fd960b49ff7ad
Oguz Kocer <oguz.kocer@automattic.com>
2021-07-12 11:38:50 -0400

    Bump version number

--- fastlane/download_metadata.swift
+++ fastlane/download_metadata.swift
@@ -3,7 +3,7 @@
 import Foundation
 
 let glotPressSubtitleKey = "app_store_subtitle"
-let glotPressWhatsNewKey = "v4.39-whats-new"
+let glotPressWhatsNewKey = "v4.40-whats-new"
 let glotPressDescriptionKey = "app_store_desc"
 let glotPressKeywordsKey = "app_store_keywords"
 let baseFolder = "./metadata"
