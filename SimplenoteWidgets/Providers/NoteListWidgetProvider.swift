a05316f8cec3cb5280ed1c748634c1ed999a39c4
Charlie Scheer <charlie.scheer@automattic.com>
2021-07-14 14:27:26 +1200

    Provided better default content for widgets

--- SimplenoteWidgets/Providers/NoteListWidgetProvider.swift
+++ SimplenoteWidgets/Providers/NoteListWidgetProvider.swift
@@ -11,11 +11,11 @@ struct NoteListWidgetProvider: IntentTimelineProvider {
     typealias Entry = NoteListWidgetEntry
 
     func placeholder(in context: Context) -> NoteListWidgetEntry {
-        return NoteListWidgetEntry(date: Date(), title: "Placeholder", noteList: ["Body Placeholder"])
+        return NoteListWidgetEntry(date: Date(), title: "Composition", noteList: Constants.noteList)
     }
 
     func getSnapshot(for configuration: ListWidgetIntent, in context: Context, completion: @escaping (NoteListWidgetEntry) -> Void) {
-        let entry = NoteListWidgetEntry(date: Date(), title: "Placeholder", noteList: ["Body Placeholder"])
+        let entry = NoteListWidgetEntry(date: Date(), title: "Composition", noteList: Constants.noteList)
 
         completion(entry)
     }
@@ -39,4 +39,17 @@ struct NoteListWidgetProvider: IntentTimelineProvider {
 
         completion(timeline)
     }
+}
+
+private struct Constants {
+    static let noteList = [
+        "Twelve Tone Serialism",
+        "Lorem Ipsum",
+        "Post Draft",
+        "Meeting Notes, Apr 21",
+        "Brain Anatomy",
+        "Color Quotes",
+        "The Valet’s Tragedy",
+        "Lorem Ipsum"
+    ]
 }
--- SimplenoteWidgets/Providers/NotePreviewWidgetProvider.swift
+++ SimplenoteWidgets/Providers/NotePreviewWidgetProvider.swift
@@ -11,26 +11,25 @@ struct NotePreviewWidgetProvider: IntentTimelineProvider {
     typealias Entry = NotePreviewWidgetEntry
 
     func placeholder(in context: Context) -> NotePreviewWidgetEntry {
-        return NotePreviewWidgetEntry(date: Date(), title: "Placeholder", body: "Body Placeholder")
+        return NotePreviewWidgetEntry(date: Date(), title: Constants.title, body: Constants.body)
     }
 
     func getSnapshot(for configuration: NoteWidgetIntent, in context: Context, completion: @escaping (NotePreviewWidgetEntry) -> Void) {
-        let entry = NotePreviewWidgetEntry(date: Date(), title: "Placeholder", body: "Body Placehoder")
+        let entry = NotePreviewWidgetEntry(date: Date(), title: Constants.title, body: Constants.body)
 
         completion(entry)
     }
 
     func getTimeline(for configuration: NoteWidgetIntent, in context: Context, completion: @escaping (Timeline<NotePreviewWidgetEntry>) -> Void) {
-        guard let note = configuration.note else {
-            return
-        }
 
-        let title = "Twelve Tone Serialism"
-        let body = "The twelve-tone technique is often closely related with the compositional style, serialism. Fundamentally, twelve-tone serialism is a compositional technique where all 12 notes of the chromatic scale are played with equal frequency throughout the piece without any emphasis on any one note. For this reason, twelve-tone serialism avoids being in any key. Arnold Schoenberg was arguably the most influential composers who embraced the twelve-tone technique. Schoenberg described the system as a “Method of composing with twelve tones which are related only with one another.”"
-
-        let entry = NotePreviewWidgetEntry(date: Date(), title: title, body: body )
+        let entry = NotePreviewWidgetEntry(date: Date(), title: Constants.title, body: Constants.body )
         let timeline = Timeline(entries: [entry], policy: .never)
 
         completion(timeline)
     }
+}
+
+private struct Constants {
+    static let title = "Twelve Tone Serialism"
+    static let body = "The twelve-tone technique is often closely related with the compositional style, serialism. Fundamentally, twelve-tone serialism is a compositional technique where all 12 notes of the chromatic scale are played with equal frequency throughout the piece without any emphasis on any one note. For this reason, twelve-tone serialism avoids being in any key. Arnold Schoenberg was arguably the most influential composers who embraced the twelve-tone technique. Schoenberg described the system as a “Method of composing with twelve tones which are related only with one another.”"
 }
