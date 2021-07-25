071009dc931e2bb7d116bdcd6082ff7e4d39e987
Charlie Scheer <charlie.scheer@automattic.com>
2021-07-26 11:29:25 +1200

    Create proof of concept core data implementation

--- SimplenoteIntents/IntentHandler.swift
+++ SimplenoteIntents/IntentHandler.swift
@@ -1,4 +1,5 @@
 import Intents
+import CoreData
 
 class IntentHandler: INExtension {
     override func handler(for intent: INIntent) -> Any {
@@ -8,15 +9,28 @@ class IntentHandler: INExtension {
 
 extension IntentHandler: NoteWidgetIntentHandling {
     func provideNoteOptionsCollection(for intent: NoteWidgetIntent, with completion: @escaping (INObjectCollection<WidgetNote>?, Error?) -> Void) {
-        // This is placeholder code to confirm the dynamic intent selection is working
-        // TODO: add logic to fetch the available notes for account
+        // NOTE: This core data implementation is just to prove that the shared database is accessible and is not intended to be used in production.
+        
+        let coreDataManager = try? CoreDataManager(StorageSettings().sharedStorageURL)
+        coreDataManager?.managedObjectContext.persistentStoreCoordinator = coreDataManager?.persistentStoreCoordinator
+        let entityDescription = NSEntityDescription.entity(forEntityName: String(describing: Note.self), in: (coreDataManager?.managedObjectContext)!)
+        let fetchRequest = NSFetchRequest<Note>()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \Note.simperiumKey, ascending: true)]
+        fetchRequest.entity = entityDescription
 
-        let placeholderNoteData = ["Note 1", "Note 2", "Note 3", "Note 4"]
+
+        let fetchedNotes = try? coreDataManager?.managedObjectContext.fetch(fetchRequest)
 
         var notes: [WidgetNote] = []
 
-        for placeholder in placeholderNoteData {
-            let spNote = WidgetNote(identifier: placeholder, display: placeholder)
+        guard let unwrappedNotes = fetchedNotes else {
+            let error = NSError(domain: "Couldn't unwrap", code: 0, userInfo: nil)
+            completion(nil, error)
+            return
+        }
+
+        for fetchedNote in unwrappedNotes {
+            let spNote = WidgetNote(identifier: fetchedNote.simperiumKey, display: fetchedNote.content ?? "Untitled Note")
             notes.append(spNote)
         }
 
