f438c6adf53e9f3d6671e29a99627cc12e300ccb
Charlie Scheer <charlie.scheer@automattic.com>
2021-08-02 15:06:15 +1200

    Updated note widget ui to design spec

--- SimplenoteWidgets/Views/NoteWidgetView.swift
+++ SimplenoteWidgets/Views/NoteWidgetView.swift
@@ -3,21 +3,40 @@ import WidgetKit
 
 struct NoteWidgetView: View {
     var entry: NoteWidgetEntry
+    @Environment(\.widgetFamily) var widgetFamily
+
     var body: some View {
-        VStack {
-            Text(entry.title)
-            Text(entry.content)
+        GeometryReader { geometry in
+            VStack {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading) {
+                        Text(entry.title)
+                            .font(widgetFamily == .systemSmall ? .subheadline : .body)
+                            .fontWeight(.bold)
+                            .padding(.bottom, Sizes.headerPadding)
+                        Text(entry.content)
+                            .font(.subheadline)
+                    }
+                    .padding([.leading, .trailing, .top], Sizes.overallPadding)
+                }
+                .ignoresSafeArea()
+            }
         }
     }
+}
+
+private struct Sizes {
+    static let overallPadding = CGFloat(20)
+    static let headerPadding = CGFloat(10)
 }
 
 struct NoteWidgetView_Previews: PreviewProvider {
     static var previews: some View {
 
         Group {
-            NoteWidgetView(entry: NoteWidgetEntry(date: Date(), title: "Title", content: "Content"))
+            NoteWidgetView(entry: NoteWidgetEntry(date: Date(), title: DemoContent.singleNoteTitle, content: DemoContent.singleNoteContent))
                 .previewContext(WidgetPreviewContext(family: .systemSmall))
-            NoteWidgetView(entry: NoteWidgetEntry(date: Date(), title: "Title", content: "Content"))
+            NoteWidgetView(entry: NoteWidgetEntry(date: Date(), title: DemoContent.singleNoteTitle, content: DemoContent.singleNoteContent))
                 .previewContext(WidgetPreviewContext(family: .systemMedium))
         }
     }
