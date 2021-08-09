e69463450e8f7251345c447824e88baef1464fe9
Charlie Scheer <charlie.scheer@automattic.com>
2021-08-09 15:29:03 +1200

    Fixed an issue where the list would crash if too few notes for tag

--- SimplenoteWidgets/Views/NoteListTable.swift
+++ SimplenoteWidgets/Views/NoteListTable.swift
@@ -8,12 +8,21 @@ struct NoteListTable: View {
     var body: some View {
         let width = geometry.size.width - Constants.sidePadding
         let height = (geometry.size.height - Constants.headerSize) / CGFloat(numberOfRows)
-        ForEach(.zero ..< numberOfRows) { index in
+        let entryCount = entry.noteProxys.count
+
+        ForEach(.zero ..< entryCount) { index in
             let proxy = entry.noteProxys[index]
             Link(destination: proxy.url) {
                 NoteListRow(noteTitle: proxy.title, width: width, height: height)
             }
-         }
+        }
+
+        if entryCount < numberOfRows {
+            let remainder = numberOfRows - entryCount
+            ForEach(.zero ..< remainder) { index in
+                NoteListRow(noteTitle: "", width: width, height: height)
+            }
+        }
     }
 }
 
