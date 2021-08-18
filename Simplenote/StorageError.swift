acc1b127e87597d468c0e0f6afa0b362a30c2e4c
Charlie Scheer <charlie.scheer@automattic.com>
2021-08-19 11:40:46 +1200

    Refactored StorageError to WidgetError

--- Simplenote/StorageError.swift
+++ Simplenote/StorageError.swift
@@ -1,20 +1,17 @@
 import Foundation
 
-enum StorageError: Error {
+enum WidgetError: Error {
     case appConfigurationError
-    case attachingPersistentStoreFailure
     case fetchError
 }
 
-extension StorageError {
+extension WidgetError {
     /// Returns the Error Title, for Alert purposes
     ///
     var title: String {
         switch self {
         case .appConfigurationError:
             return NSLocalizedString("Main App is not configured", comment: "App configuration error title")
-        case .attachingPersistentStoreFailure:
-            return NSLocalizedString("Could not attach Persistent Store", comment: "Persistent Store error title")
         case .fetchError:
             return NSLocalizedString("Could not fetch entities", comment: "Fetch error title")
         }
@@ -26,8 +23,6 @@ extension StorageError {
         switch self {
         case .appConfigurationError:
             return NSLocalizedString("Simplenote must be configured and logged in to setup widgets", comment: "Message displayed when app is not configured")
-        case .attachingPersistentStoreFailure:
-            return NSLocalizedString("Failed to load persistent store to coordinator", comment: "Message displayed when persistent store load fails")
         case .fetchError:
             return NSLocalizedString("Attempt to fetch entities from core data failed", comment: "Data Fetch error message")
         }
