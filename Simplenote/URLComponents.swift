cb5a51cb5f1b67445c4be1875f38c246877a45e2
Charlie Scheer <charlie.scheer@automattic.com>
2021-08-31 13:57:11 +1200

    Refactored Simplenote URL components method

--- Simplenote/URLComponents.swift
+++ Simplenote/URLComponents.swift
@@ -3,10 +3,7 @@ import Foundation
 extension URLComponents {
     static func simplenoteURLComponents(with host: String? = nil) -> URLComponents? {
         var components = URLComponents(string: .simplenotePath())
-
-        if let host = host {
-            components?.host = host
-        }
+        components?.host = host
 
         return components
     }
