a819affde5fd13bb75d2064a0729a2e498f0de92
Charlie Scheer <charlie.scheer@automattic.com>
2021-09-02 12:35:24 +1200

    Removed some duplication by adding NSString extensions to widgets

--- Simplenote/Classes/String+Simplenote.swift
+++ Simplenote/Classes/String+Simplenote.swift
@@ -209,12 +209,3 @@ extension String {
         return components.joined(separator: " ")
     }
 }
-
-// MARK: Tag hash encoding
-extension String {
-    var byEncodingAsTagHash: String {
-        precomposedStringWithCanonicalMapping
-            .lowercased()
-            .addingPercentEncoding(withAllowedCharacters: .alphanumerics) ?? self as String
-    }
-}
