33998c6593768bed2b63e7887ecc57024b7c826f
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2022-07-15 14:56:23 -0600

    Add installable builds

---
+++ .buildkite/commands/build-prototype.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+set -eu
+
+# FIXIT-13.1: Temporary fix until we're on the Xcode 13.1 VM
+echo "--- :rubygems: Fixing Ruby Setup"
+gem install bundler
+
+echo "--- :rubygems: Setting up Gems"
+install_gems
+
+echo "--- :cocoapods: Setting up Pods"
+install_cocoapods
+
+echo "--- Installing Secrets"
+bundle exec fastlane run configure_apply
+
+echo "--- :hammer_and_wrench: Build and Test"
+bundle exec fastlane build_and_upload_installable_build build_number:$BUILDKITE_BUILD_NUMBER
