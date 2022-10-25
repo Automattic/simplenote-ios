6f4567000aa72db3451d86a134698ef9a84177cc
Jorge Leandro Perez <jorge@lantean.co>
2022-10-25 14:29:37 -0300

    Implements SustainerView

---
+++ Simplenote/SustainerView.swift
@@ -0,0 +1,30 @@
+import Foundation
+import UIKit
+
+
+// MARK: - SustainerView
+//
+class SustainerView: UIView {
+
+    @IBOutlet
+    private var backgroundView: UIView!
+
+    @IBOutlet
+    private var titleLabel: UILabel!
+
+    @IBOutlet
+    private var detailsLabel: UILabel!
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundView.layer.cornerRadius = 8
+        backgroundView.backgroundColor = .simplenoteBlue50Color
+
+        titleLabel.text = NSLocalizedString("Become a Simplenote Sustainer", comment: "Sustainer Title")
+        detailsLabel.text = NSLocalizedString("Support your favorite notes app to help unlock future features", comment: "Sustainer Legend")
+
+        titleLabel.textColor = .white
+        detailsLabel.textColor = .white
+    }
+}
