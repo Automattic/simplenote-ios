685e98914fc8d1cd2c16ad329e97517fe13df971
Jorge Leandro Perez <jorge@lantean.co>
2022-10-26 15:46:09 -0300

    Preferences: New Properties

--- Simplenote/Preferences.h
+++ Simplenote/Preferences.h
@@ -4,6 +4,10 @@
 @interface Preferences: SPManagedObject
 
 @property (nullable, nonatomic, strong) NSArray<NSString *> *recent_searches;
-@property (nullable, nonatomic, copy) NSNumber              *analytics_enabled;
+@property (nullable, nonatomic,   copy) NSNumber            *analytics_enabled;
+
+@property (nullable, nonatomic,   copy) NSDate              *subscription_date;
+@property (nullable, nonatomic,   copy) NSString            *subscription_level;
+@property (nullable, nonatomic,   copy) NSString            *subscription_platform;
 
 @end
