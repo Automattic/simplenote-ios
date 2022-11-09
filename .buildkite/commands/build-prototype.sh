7c6ea1b8dcfe2f292d497d23fbc15d03e29466b2
Spencer Transier <spencer@transier.us>
2022-11-09 14:23:46 -0700

    Remove unneeded test result reporting for installable

--- .buildkite/commands/build-prototype.sh
+++ .buildkite/commands/build-prototype.sh
@@ -9,28 +9,5 @@ install_cocoapods
 echo "--- Installing Secrets"
 bundle exec fastlane run configure_apply
 
-echo "--- :hammer_and_wrench: Build and Test"
-set +e
+echo "--- :hammer_and_wrench: Build and Upload to App Center"
 bundle exec fastlane build_and_upload_installable_build
-TESTS_EXIT_STATUS=$?
-set -e
-
-if [[ $TESTS_EXIT_STATUS -ne 0 ]]; then
-  # Keep the (otherwise collapsed) current "Testing" section open in Buildkite logs on error. See https://buildkite.com/docs/pipelines/managing-log-output#collapsing-output
-  echo "^^^ +++"
-  echo "UI Tests failed!"
-fi
-
-echo "--- 📦 Zipping test results"
-cd build/results/ && zip -rq Simplenote.xcresult.zip Simplenote.xcresult && cd -
-
-echo "--- 🚦 Report Tests Status"
-if [[ $TESTS_EXIT_STATUS -eq 0 ]]; then
-  echo "Unit Tests seems to have passed (exit code 0). All good 👍"
-else
-  echo "The UI Tests, ran during the '🛠️ Build and Test' step above, have failed."
-  echo "For more details about the failed tests, check the Buildkite annotation, the logs under the '🛠️ Build and Test' section and the \`.xcresult\` and test reports in Buildkite artifacts."
-fi
-annotate_test_failures "build/results/report.junit"
-
-exit $TESTS_EXIT_STATUS
