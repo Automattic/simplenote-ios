4003f6740a859233fb2112378e42ef56506ad023
Gio Lodi <gio@mokacoding.com>
2023-04-13 20:15:00 +1000

    Update CI release automation to not require user input
    This must have been something lost in translation when moving from
    CircleCI to Buildkite. Not surprising this wasn't noticed given that
    Simplenote iOS is in maintenance mode.

--- .buildkite/commands/release-build.sh
+++ .buildkite/commands/release-build.sh
@@ -10,4 +10,8 @@ echo "--- :closed_lock_with_key: Installing Secrets"
 bundle exec fastlane run configure_apply
 
 echo "--- :hammer_and_wrench: Build and Upload to App Store Connect"
-bundle exec fastlane build_and_upload_to_app_store_connect
+bundle exec fastlane build_and_upload_to_app_store_connect \
+  skip_confirm:true \
+  create_release:true \
+  skip_prechecks:true \
+  beta_release:${1:-true} # use first call param, default to true for safety
