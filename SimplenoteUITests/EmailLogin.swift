b00004ef246f907dad532ddb19a04e6287e9a29b
pachlava <sergiy.fedosov@automattic.com>
2023-11-02 20:18:24 +0200

    Handle "Save Password" prompt from iOS.

--- SimplenoteUITests/EmailLogin.swift
+++ SimplenoteUITests/EmailLogin.swift
@@ -12,6 +12,7 @@ class EmailLogin {
         guard backButton.isHittable else { return }
 
         backButton.tap()
+        handleSavePasswordPrompt()
     }
 
     class func logIn() {
@@ -33,6 +34,7 @@ class EmailLogin {
         enterEmail(enteredValue: email)
         enterPassword(enteredValue: password)
         app.buttons[UID.Button.logIn].tap()
+        handleSavePasswordPrompt()
     }
 
     class func enterEmail(enteredValue: String) {
@@ -45,5 +47,14 @@ class EmailLogin {
         let field = app.secureTextFields[UID.TextField.password]
         field.tap()
         field.typeText(enteredValue)
+    }
+
+    class func handleSavePasswordPrompt() {
+        // As of Xcode 14.3, the Simulator might ask to save the password which, of course, we don't want to do.
+        if app.buttons["Save Password"].waitForExistence(timeout: 5) {
+            // There should be no need to wait for this button to exist since it's part of the same
+            // alert where "Save Password" is.
+            app.buttons["Not Now"].tap()
+        }
     }
 }
