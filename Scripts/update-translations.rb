38fceee8467d7629c48025acc7ece1c9955a77c1
Spencer Transier <spencer.transier@automattic.com>
2024-02-06 22:27:45 -0800

    Fix Rubocop violations

--- Scripts/update-translations.rb
+++ Scripts/update-translations.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
-# encoding: utf-8
+# frozen_string_literal: true
 
 # Supported languages:
-# ar,cy,zh-Hans,zh-Hant,nl,fa,fr,de,el,he,id,ko,pt,ru,es,sv,tr,ja,it 
+# ar,cy,zh-Hans,zh-Hant,nl,fa,fr,de,el,he,id,ko,pt,ru,es,sv,tr,ja,it
 # * Arabic
 # * Welsh
 # * Chinese (China) [zh-Hans]
@@ -25,11 +25,11 @@
 require 'json'
 
 if Dir.pwd =~ /Scripts/
-  puts "Must run script from root folder"
+  puts 'Must run script from root folder'
   exit
 end
 
-ALL_LANGS={
+ALL_LANGS = {
   'ar' => 'ar',         # Arabic
   'cy' => 'cy',         # Welsh
   'de' => 'de',         # German
@@ -48,34 +48,34 @@ ALL_LANGS={
   'sv' => 'sv',         # Swedish
   'tr' => 'tr',         # Turkish
   'zh-cn' => 'zh-Hans-CN', # Chinese (China)
-  'zh-tw' => 'zh-Hant-TW', # Chinese (Taiwan)
-}
+  'zh-tw' => 'zh-Hant-TW' # Chinese (Taiwan)
+}.freeze
 
 def copy_header(target_file, trans_strings)
   trans_strings.each_line do |line|
     if (!line.start_with?("/*"))
       target_file.write("\n")
       return
-    end 
+    end
 
     target_file.write(line)
   end
 end
 
 def copy_comment(f, trans_strings, value)
-  prev_line=""
+  prev_line = ''
   trans_strings.each_line do |line|
     if line.include?(value)
       f.write(prev_line)
-      return 
+      return
     end
-    prev_line=line
+    prev_line = line
   end
 end
 
 langs = {}
-if ARGV.count > 0
-  for key in ARGV
+if ARGV.count.positive?
+  ARGV.each do |key|
     unless local = ALL_LANGS[key]
       puts "Unknown language #{key}"
       exit 1
@@ -86,11 +86,11 @@ else
   langs = ALL_LANGS
 end
 
-langs.each do |code,local|
+langs.each do |code, local|
   lang_dir = File.join('Simplenote', "#{local}.lproj")
   puts "Updating #{code}"
   system "mkdir -p #{lang_dir}"
-  
+
   # Backup the current file
   system "if [ -e #{lang_dir}/Localizable.strings ]; then cp #{lang_dir}/Localizable.strings #{lang_dir}/Localizable.strings.bak; fi"
 
