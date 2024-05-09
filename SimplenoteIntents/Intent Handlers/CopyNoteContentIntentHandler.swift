647d41f1d761069bc9e593e24ce1d12a2c5b846f
Charlie Scheer <charlie.scheer@automattic.com>
2024-05-09 15:57:25 -0600

    Added copy note content intent handler

---
+++ SimplenoteIntents/Intent Handlers/CopyNoteContentIntentHandler.swift
@@ -0,0 +1,9 @@
+//
+//  CopyNoteContentIntentHandler.swift
+//  SimplenoteIntents
+//
+//  Created by Charlie Scheer on 5/9/24.
+//  Copyright © 2024 Automattic. All rights reserved.
+//
+
+import Foundation
