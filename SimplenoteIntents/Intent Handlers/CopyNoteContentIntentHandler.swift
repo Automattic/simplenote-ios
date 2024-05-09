58c13b46c10cfd2282a56b8b4716dbc922e0849f
Charlie Scheer <charlie.scheer@automattic.com>
2024-05-09 15:57:40 -0600

    Created intent handler to copy content from a note

--- SimplenoteIntents/Intent Handlers/CopyNoteContentIntentHandler.swift
+++ SimplenoteIntents/Intent Handlers/CopyNoteContentIntentHandler.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2024 Automattic. All rights reserved.
 //
 
-import Foundation
+import Intents
+
+class CopyNoteContentIntentHandler: NSObject, CopyNoteContentIntentHandling {
+    let coreDataWrapper = ExtensionCoreDataWrapper()
+
+    func provideNoteOptionsCollection(for intent: CopyNoteContentIntent) async throws -> INObjectCollection<IntentNote> {
+        let intentNotes = try IntentNote.allNotes(in: coreDataWrapper)
+        return INObjectCollection(items: intentNotes)
+    }
+
+    func handle(intent: CopyNoteContentIntent) async -> CopyNoteContentIntentResponse {
+        guard let note = intent.note else {
+            return CopyNoteContentIntentResponse(code: .unspecified, userActivity: nil)
+        }
+
+        guard let identifier = note.identifier,
+              let content = coreDataWrapper.resultsController()?.note(forSimperiumKey: identifier)?.content else {
+            return CopyNoteContentIntentResponse(code: .failure, userActivity: nil)
+        }
+
+        let response = CopyNoteContentIntentResponse(code: .success, userActivity: nil)
+        response.noteContent = content
+        return response
+    }
+}
