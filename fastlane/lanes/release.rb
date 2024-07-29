eec335aac3498554b103dc7ff6c5853a07ae9660
Gio Lodi <gio@mokacoding.com>
2024-07-29 10:54:08 +1000

    Introduce version calculators and adopt in code freeze lane

--- fastlane/lanes/release.rb
+++ fastlane/lanes/release.rb
@@ -8,8 +8,34 @@ platform :ios do
 
     Fastlane::Helper::GitHelper.checkout_and_pull(DEFAULT_BRANCH)
 
-    ios_bump_version_release
-    new_version = ios_get_app_version(public_version_xcconfig_file: VERSION_FILE_PATH)
+    computed_release_branch_name = release_branch_name(release_version: release_version_next)
+
+    message = <<~MESSAGE
+      Code Freeze:
+      • New release branch from #{DEFAULT_BRANCH}: #{computed_release_branch_name}
+
+      • Current release version and build code: #{release_version_current} (#{build_code_current}).
+      • New release version and build code: #{release_version_next} (#{build_code_code_freeze}).
+    MESSAGE
+
+    UI.important(message)
+
+    UI.user_error!('Aborted by user request') unless skip_confirm || UI.confirm('Do you want to continue?')
+
+    UI.message 'Creating release branch...'
+    Fastlane::Helper::GitHelper.create_branch(computed_release_branch_name, from: DEFAULT_BRANCH)
+    UI.success "Done! New release branch is: #{git_branch}"
+
+    UI.message 'Bumping release version and build code...'
+    PUBLIC_VERSION_FILE.write(
+      version_short: release_version_next,
+      version_long: build_code_code_freeze
+    )
+    UI.success "Done! New Release Version: #{release_version_current}. New Build Code: #{build_code_current}."
+
+    commit_version_and_build_files
+
+    new_version = release_version_current
 
     extract_release_notes_for_version(
       version: new_version,
@@ -20,7 +46,7 @@ platform :ios do
 
     generate_strings_file_for_glotpress
 
-    UI.important('Pushing changes to remote, configuring the release on GitHub, and triggering the beta build')
+    UI.important('Pushing changes to remote, configuring the release on GitHub, and triggering the beta build...')
     UI.user_error!("Terminating as requested. Don't forget to run the remainder of this automation manually.") unless skip_confirm || UI.confirm('Do you want to continue?')
 
     push_to_git_remote(tags: false)
@@ -28,14 +54,14 @@ platform :ios do
     copy_branch_protection(
       repository: GITHUB_REPO,
       from_branch: DEFAULT_BRANCH,
-      to_branch: "release/#{new_version}"
+      to_branch: computed_release_branch_name
     )
     set_milestone_frozen_marker(
       repository: GITHUB_REPO,
       milestone: new_version
     )
 
-    trigger_beta_build(branch_to_build: "release/#{new_version}")
+    trigger_beta_build(branch_to_build: computed_release_branch_name)
   end
 
   lane :new_beta_release do |options|
@@ -63,4 +89,12 @@ platform :ios do
       beta: false
     )
   end
+end
+
+def commit_version_and_build_files
+  git_commit(
+    path: [PUBLIC_CONFIG_FILE],
+    message: 'Bump version number',
+    allow_nothing_to_commit: false
+  )
 end
