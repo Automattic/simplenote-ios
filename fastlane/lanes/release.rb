3c9907698e74fa51c5fd3fb698cbc71f92773c1c
Gio Lodi <gio@mokacoding.com>
2024-08-30 11:15:51 +1000

    Move `finalize_release` lane to dedicated file and modernize

--- fastlane/lanes/release.rb
+++ fastlane/lanes/release.rb
@@ -103,6 +103,77 @@ platform :ios do
     # TODO: Switch to working branch and open back-merge PR
   end
 
+  desc 'Trigger the final release build on CI'
+  lane :finalize_release do |skip_confirm: false|
+    UI.user_error!('To finalize a hotfix, please use the finalize_hotfix_release lane instead') if ios_current_branch_is_hotfix
+
+    ensure_git_status_clean
+    ensure_git_branch_is_release_branch!
+
+    check_translation_progress
+
+    new_build_code = build_code_next
+    version = release_version_current
+    UI.important <<~MESSAGE
+      Finalizing release #{version}:
+      • Current build code: #{build_code_current}
+      • Final build code: #{new_build_code}
+    MESSAGE
+
+    UI.user_error!("Terminating as requested. Don't forget to run the remainder of this automation manually.") unless skip_confirm || UI.confirm('Do you want to continue?')
+
+    download_localized_strings_and_metadata_from_glotpress
+    lint_localizations
+
+    UI.message "Bumping build code to #{new_build_code}..."
+    PUBLIC_VERSION_FILE.write(version_long: new_build_code)
+    commit_version_and_build_files
+    # Uses build_code_current let user double-check result.
+    UI.success "Done! Release version: #{version}. Final build code: #{build_code_current}."
+
+    UI.important('Will push changes to remote and trigger the release build.')
+    UI.user_error!("Terminating as requested. Don't forget to run the remainder of this automation manually.") unless skip_confirm || UI.confirm('Do you want to continue?')
+
+    push_to_git_remote(tags: false)
+
+    trigger_release_build(branch_to_build: release_branch_name)
+
+    remove_branch_protection(
+      repository: GITHUB_REPO,
+      branch: release_branch_name
+    )
+
+    begin
+      set_milestone_frozen_marker(
+        repository: GITHUB_REPO,
+        milestone: version,
+        freeze: false
+      )
+
+      create_new_milestone(repository: GITHUB_REPO)
+
+      close_milestone(
+        repository: GITHUB_REPO,
+        milestone: version
+      )
+    rescue StandardError => e
+      report_milestone_error(error_title: "Error in milestone finalization process for `#{version}`: #{e.message}")
+    end
+
+    UI.message("Moved the following PRs to milestone #{release_version_next}: #{moved_prs.join(', ')}")
+
+    next unless is_ci
+
+    moved_prs_info = if moved_prs.empty?
+                       "No open PR were targeting `#{new_version}` at the time of code-freeze."
+                     else
+                       "#{moved_prs.count} PRs targeting `#{new_version}` were still open and thus moved to `#{release_version_next}`:\n" \
+                         + moved_prs.map { |pr_num| "[##{pr_num}](https://github.com/#{GITHUB_REPO}/pull/#{pr_num})" }.join(', ')
+                     end
+
+    buildkite_annotate(style: moved_prs.empty? ? 'success' : 'warning', context: 'start-code-freeze', message: moved_prs_info)
+  end
+
   lane :trigger_beta_build do |branch_to_build:|
     trigger_buildkite_release_build(branch: branch_to_build, beta: true)
   end
@@ -137,4 +208,16 @@ def trigger_buildkite_release_build(branch:, beta:)
     environment: { BETA_RELEASE: beta },
     pipeline_file: 'release-build.yml'
   )
+end
+
+def report_milestone_error(error_title:)
+  error_message = <<-MESSAGE
+    #{error_title}
+    - If this is not the first time you are running the release task (e.g. retrying because it failed on first attempt), the milestone might have already been closed and this error is expected.
+    - Otherwise, please investigate the error.
+  MESSAGE
+
+  UI.error(error_message)
+
+  buildkite_annotate(style: 'warning', context: 'error-with-milestone', message: error_message) if is_ci
 end
