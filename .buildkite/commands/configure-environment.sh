ad1541176bef7d0b6e80878f15911af27afa4370
Gio Lodi <gio@mokacoding.com>
2024-09-04 15:15:12 +1000

    Inlinie `configure-environment.sh` script

--- .buildkite/commands/configure-environment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eu
-
-echo '--- :git: Configure Git for Release Management'
-.buildkite/commands/configure-git-for-release-management.sh
-
-echo '--- :ruby: Set up Ruby Tools'
-install_gems
