b07ebc42f4f789e1ebb33b377778318c65ce6f86
Gio Lodi <gio@mokacoding.com>
2024-09-04 14:49:26 +1000

    Remove the need for the `RELEASE_NUMBER` argument

--- .buildkite/commands/complete-code-freeze.sh
+++ .buildkite/commands/complete-code-freeze.sh
@@ -1,17 +1,11 @@
 #!/bin/bash -eu
 
-RELEASE_NUMBER=$1
-
-if [[ -z "${RELEASE_NUMBER}" ]]; then
-    echo "Usage $0 <release number>"
-    exit 1
-fi
 
 echo '--- :git: Configure Git for release management'
 .buildkite/commands/configure-git-for-release-management.sh
 
 echo '--- :git: Checkout release branch'
-.buildkite/commands/checkout-release-branch.sh "$RELEASE_NUMBER"
+.buildkite/commands/checkout-release-branch.sh
 
 echo '--- :ruby: Setup Ruby tools'
 install_gems
