bba6f08c82aa146745d7c88a5ad232f434da7a01
Charlie Scheer <charlie.scheer@automattic.com>
2024-12-29 10:44:13 -1000

    Fixed highlighting to work case unsensitive

--- Simplenote/SearchHighlightableTextParagraph.swift
+++ Simplenote/SearchHighlightableTextParagraph.swift
@@ -2,20 +2,20 @@ import Foundation
 
 class SearchHighlightableTextParagraph: NSTextParagraph {
 
-    init(attributedString: NSAttributedString, searchText: String?) {
-        guard let searchText,
-            attributedString.string.contains(searchText),
+    init(attributedString: NSAttributedString, searchText: String?, isSelected: Bool) {
+        let plainText = attributedString.string.lowercased()
+        guard let searchText = searchText?.lowercased(),
+              plainText.contains(searchText),
             let mutableString = attributedString.mutableCopy() as? NSMutableAttributedString else {
             super.init(attributedString: attributedString)
             return
         }
 
-        let range = attributedString.string.nsString.range(of: searchText)
+        let range = plainText.nsString.range(of: searchText)
         mutableString.addAttributes([
             .backgroundColor: UIColor.simplenoteEditorSearchHighlightSelectedColor,
             .foregroundColor: UIColor.simplenoteEditorSearchHighlightTextColor
         ], range: range)
-
 
         super.init(attributedString: mutableString)
     }
