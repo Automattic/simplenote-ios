28325b960f32762351fbbf997f5e35d77a100110
Gio Lodi <gio@mokacoding.com>
2025-12-01 20:41:36 +1100

    Demo extracting info from the `xcresulttool` JSON

--- .buildkite/commands/build-and-test.sh
+++ .buildkite/commands/build-and-test.sh
@@ -61,7 +61,7 @@ for file in DerivedData/Logs/Build/*.xcactivitylog; do
   echo " ✅ $filename queued"
 done
 
-echo "--- :xcode: Store raw result JSON"
+echo "--- :xcode: Store raw xcresulttool JSONs"
 mkdir -p build/xcresulttool
 xcrun xcresulttool get build-results \
   --path build/results/Simplenote.xcresult \
@@ -69,6 +69,21 @@ xcrun xcresulttool get build-results \
 xcrun xcresulttool get test-results tests \
   --path build/results/Simplenote.xcresult \
   --format json > build/xcresulttool/xcresulttool-tests-results.json
+
+echo "--- :json: Extract build info"
+jq '{
+  timestamp: .startTime | strftime("%Y-%m-%d %H:%M:%S"),
+  duration_ms: ((.endTime - .startTime) * 1000 | round),
+  status: .status,
+  errors: .errorCount,
+  warnings: .warningCount,
+  analyzer_warnings: .analyzerWarningCount,
+  warning_breakdown: (.warnings | group_by(.issueType) | map({type: .[0].issueType, count: length}) | sort_by(-.count))
+}' build/xcresulttool/xcresulttool-build-results.json
+
+echo "--- :json: Extract success/fail test count"
+jq '[.. | objects | select(has("result")) | .result] | {passed: map(select(. == "Passed")) | length, failed: map(select(. == "Failed")) | length}' \
+  build/xcresulttool/xcresulttool-tests-results.json
 
 echo "--- :xcode: Track XCLogParser report"
 echo "~~~ Install XCLogParser"
