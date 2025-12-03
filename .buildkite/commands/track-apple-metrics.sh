de900d5760141804405df351f70c4be9763af8a1
Gio Lodi <gio@mokacoding.com>
2025-12-03 17:19:23 +1100

    Add Chrome tracer JSON report

--- .buildkite/commands/track-apple-metrics.sh
+++ .buildkite/commands/track-apple-metrics.sh
@@ -12,11 +12,13 @@ mkdir -p build/xcresulttool
 xcrun xcresulttool get build-results \
   --path "$XCRESULT_PATH" \
   --format json > build/xcresulttool/xcresulttool-build-results.json
+
 buildkite-agent artifact upload "build/xcresulttool/xcresulttool-build-results.json"
 
 xcrun xcresulttool get test-results tests \
   --path "$XCRESULT_PATH" \
   --format json > build/xcresulttool/xcresulttool-tests-results.json
+
 buildkite-agent artifact upload "build/xcresulttool/xcresulttool-tests-results.json"
 
 echo "+++ :json: Extract build info from xcresulttool"
@@ -35,25 +37,36 @@ jq '[.. | objects | select(has("result")) | .result] | {passed: map(select(. == 
   build/xcresulttool/xcresulttool-tests-results.json
 
 echo "--- :xcode: Track XCLogParser report"
+
 echo "~~~ Install XCLogParser"
+
 brew install xclogparser
+
 mkdir -p build/xclogparser-reports
+
 echo "~~~ Dump xcactivitylog to JSON"
+
 xclogparser dump \
   --xcodeproj Simplenote.xcodeproj \
   --derived_data "$DERIVED_DATA_PATH" \
   --output build/xclogparser-reports/xcactivitylog-raw.json
+
 buildkite-agent artifact upload "build/xclogparser-reports/xcactivitylog-raw.json"
-echo "~~~ Generate HTML report"
-xclogparser parse \
-  --xcodeproj Simplenote.xcodeproj \
-  --derived_data "$DERIVED_DATA_PATH" \
-  --reporter html \
-  --rootOutput build/xclogparser-reports
+
 echo "~~~ Generate JSON report"
+
 xclogparser parse \
   --xcodeproj Simplenote.xcodeproj \
   --derived_data "$DERIVED_DATA_PATH" \
   --reporter json > build/xclogparser-reports/report.json
+
 buildkite-agent artifact upload "build/xclogparser-reports/report.json"
 
+echo "~~~ Generate Chrome tracer report"
+
+xclogparser parse \
+  --xcodeproj Simplenote.xcodeproj \
+  --derived_data "$DERIVED_DATA_PATH" \
+  --reporter chromeTracer > build/xclogparser-reports/build-trace.json
+
+buildkite-agent artifact upload "build/xclogparser-reports/build-trace.json"
